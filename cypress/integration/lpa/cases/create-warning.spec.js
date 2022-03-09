fe6458dd8f028f5fc2c7b681c3635a854da984d3
Joshua Hawxwell <m@hawx.me>
2022-03-09 15:10:38 +0000

    Add test to create a warning with iframe

---
+++ cypress/integration/lpa/cases/create-warning.spec.js
@@ -0,0 +1,31 @@
+describe('Create a warning', { tags: ["@lpa", "@smoke-journey"] }, () => {
+  before(() => {
+    cy.loginAs('LPA Manager');
+    cy.createDonor().then((donorId) => {
+      cy.createLpa(donorId).then((lpaId) => {
+        cy.visit(`/lpa/#/person/${donorId}/${lpaId}`);
+      });
+    });
+  });
+
+  it('should show the warning and a timeline event', () => {
+    cy.intercept({ method: 'GET', url: '/api/v1/persons/*' }).as('personRequest');
+    cy.intercept({ method: 'GET', url: '/api/v1/persons/*/warnings' }).as('warningsRequest');
+
+    cy.wait(['@personRequest', '@warningsRequest']);
+
+    cy.contains('Create Warning').click();
+
+    cy.frameLoaded('.action-widget-content iframe');
+    cy.enter('.action-widget-content iframe').then(getBody => {
+      getBody().find('#f-warningType').select('Fee Issue');
+      getBody().find('#f-warningText').type('This is a big problem');
+      getBody().find('button[type=submit]').click();
+    });
+
+    cy.wait('@warningsRequest');
+
+    cy.contains('.warning-item', 'Fee Issue').should('contain', 'This is a big problem');
+    cy.contains('.timeline-event', 'Fee Issue').should('contain', 'This is a big problem');
+  });
+});
