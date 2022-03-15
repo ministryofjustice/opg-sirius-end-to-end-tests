8f5875d17dcda875180cf23a2d46b16d4bf497be
ramchale <legionsofbob@gmail.com>
2022-03-15 12:23:06 +0000

    No longer ignore uncaught exceptions

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -4,8 +4,3 @@ require('./lpa-commands');
 
 require('cypress-grep')();
 require('cypress-failed-log');
-
-// Ignore uncaught exceptions due to "process is not defined" error on the Supervision homepage
-Cypress.on('uncaught:exception', (err, runnable) => {
-  return false;
-});
