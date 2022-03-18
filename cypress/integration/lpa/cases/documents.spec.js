d345f103100fc6d7438b4c75649cd368041581b0
Joshua Hawxwell <m@hawx.me>
2022-03-18 10:04:42 +0000

    VEGA-1218 Wait for the draft to be created before checking iframe (#38)

--- cypress/integration/lpa/cases/documents.spec.js
+++ cypress/integration/lpa/cases/documents.spec.js
@@ -2,8 +2,9 @@ describe("Documents", { tags: ["@lpa", "@smoke-journey"] }, () => {
   beforeEach(() => {
     cy.intercept({ method: 'GET', url: '/api/v1/persons/*/cases' }).as('casesRequest');
     cy.intercept({ method: 'GET', url: '/api/v1/templates/lpa' }).as('templatesRequest');
-    cy.intercept({ method: 'GET', url: '/api/v1/lpas/*/draft-count' }).as('draftRequest');
+    cy.intercept({ method: 'GET', url: '/api/v1/lpas/*/draft-count' }).as('draftCountRequest');
     cy.intercept({ method: 'POST', url: '/api/v1/lpas/*/documents' }).as('documentsRequest');
+    cy.intercept({ method: 'POST', url: '/api/v1/lpas/*/documents/draft' }).as('draftRequest');
     cy.intercept({ method: 'GET', url: '/api/v1/persons/*/events?*' }).as('eventsRequest');
 
     cy.loginAs("LPA Manager");
@@ -27,6 +28,7 @@ describe("Documents", { tags: ["@lpa", "@smoke-journey"] }, () => {
     cy.contains("IT-11").click();
     cy.contains("label", "Bob Sponge").children("input").check();
     cy.contains("Generate").click();
+    cy.wait("@draftRequest");
 
     cy.frameLoaded('.tox-edit-area__iframe');
     cy.enter(".tox-edit-area__iframe").then(getBody => {
@@ -43,7 +45,7 @@ describe("Documents", { tags: ["@lpa", "@smoke-journey"] }, () => {
 
     cy.visit(`/lpa/#/person/${this.donorId}/${this.lpaId}`);
     cy.wait(["@casesRequest", "@eventsRequest"]);
-    cy.wait("@draftRequest");
+    cy.wait("@draftCountRequest");
 
     cy.get("#RetrieveDraft").click();
 
