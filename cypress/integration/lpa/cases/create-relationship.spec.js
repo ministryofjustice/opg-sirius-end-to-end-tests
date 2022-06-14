34c302e4c3bb8d788c051a9d878e7b12f661a4f3
Joshua Hawxwell <m@hawx.me>
2022-06-14 13:58:25 +0100

    Temporarily remove create relationship test (#71)

--- cypress/integration/lpa/cases/create-relationship.spec.js
+++ cypress/integration/lpa/cases/create-relationship.spec.js
@@ -1,37 +1,37 @@
-describe('Create a relationship', { tags: ["@lpa", "@smoke-journey"] }, () => {
-  before(() => {
-    cy.loginAs('LPA Manager');
-    cy.createDonor().then(({ id: donorId }) => {
-      cy.createDonor().then(({ uId: otherDonorUid }) => {
-        cy.createLpa(donorId).then(({ id: lpaId }) => {
-          cy.visit(`/lpa/#/person/${donorId}/${lpaId}`);
-          cy.wrap(otherDonorUid).as("otherDonorUid");
-        });
-      });
-    });
-  });
+// describe('Create a relationship', { tags: ["@lpa", "@smoke-journey"] }, () => {
+//   before(() => {
+//     cy.loginAs('LPA Manager');
+//     cy.createDonor().then(({ id: donorId }) => {
+//       cy.createDonor().then(({ uId: otherDonorUid }) => {
+//         cy.createLpa(donorId).then(({ id: lpaId }) => {
+//           cy.visit(`/lpa/#/person/${donorId}/${lpaId}`);
+//           cy.wrap(otherDonorUid).as("otherDonorUid");
+//         });
+//       });
+//     });
+//   });
 
-  it('should show the relationship', () => {
-    cy.intercept({ method: 'GET', url: '/*/v1/persons/*' }).as('personRequest');
-    cy.intercept({ method: 'GET', url: '/*/v1/persons/*/references' }).as('referenceRequest');
+//   it('should show the relationship', () => {
+//     cy.intercept({ method: 'GET', url: '/*/v1/persons/*' }).as('personRequest');
+//     cy.intercept({ method: 'GET', url: '/*/v1/persons/*/references' }).as('referenceRequest');
 
-    cy.wait('@personRequest');
+//     cy.wait('@personRequest');
 
-    cy.get("#Workflow").click();
-    cy.contains("Create Relationship").click();
+//     cy.get("#Workflow").click();
+//     cy.contains("Create Relationship").click();
 
-    cy.frameLoaded('.action-widget-content iframe');
-    cy.enter('.action-widget-content iframe').then(getBody => {
-      cy.get("@otherDonorUid").then(uid => {
-        getBody().find("#f-search").type(uid);
-        getBody().find(".autocomplete__menu").contains("Bob Sponge").click();
-        getBody().find("#f-reason").type("Sponge");
-        getBody().find('button[type=submit]').click();
-      });
-    });
+//     cy.frameLoaded('.action-widget-content iframe');
+//     cy.enter('.action-widget-content iframe').then(getBody => {
+//       cy.get("@otherDonorUid").then(uid => {
+//         getBody().find("#f-search").type(uid);
+//         getBody().find(".autocomplete__menu").contains("Bob Sponge").click();
+//         getBody().find("#f-reason").type("Sponge");
+//         getBody().find('button[type=submit]').click();
+//       });
+//     });
 
-    cy.wait('@referenceRequest');
+//     cy.wait('@referenceRequest');
 
-    cy.contains('.case-summary', 'This donor has a link with Bob Sponge (Sponge)');
-  });
-});
+//     cy.contains('.case-summary', 'This donor has a link with Bob Sponge (Sponge)');
+//   });
+// });
