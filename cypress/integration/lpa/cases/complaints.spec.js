bca9c0c26db664f4833dd231cd90f51726173292
Joshua Hawxwell <m@hawx.me>
2022-06-20 14:13:48 +0100

    Add test for embedded complaint form

--- cypress/integration/lpa/cases/complaints.spec.js
+++ cypress/integration/lpa/cases/complaints.spec.js
@@ -1,39 +1,48 @@
-// describe('Complaints', { tags: ["@lpa", "@smoke-journey"] }, () => {
-//   before(() => {
-//     cy.loginAs('LPA Manager');
-//     cy.createDonor().then(({ id: donorId }) => {
-//       cy.createLpa(donorId).then(({ id: lpaId }) => {
-//         cy.visit(`/lpa/#/person/${donorId}/${lpaId}`);
-//       });
-//     });
-//   });
+describe('Complaints', { tags: ["@lpa", "@smoke-journey"] }, () => {
+  before(() => {
+    cy.loginAs('LPA Manager');
+    cy.createDonor().then(({ id: donorId }) => {
+      cy.createLpa(donorId).then(({ id: lpaId }) => {
+        cy.visit(`/lpa/#/person/${donorId}/${lpaId}`);
+      });
+    });
+  });
 
-//   it('should add and edit a complaint', () => {
-//     cy.intercept({ method: 'GET', url: '/*/v1/persons/*' }).as('personRequest');
-//     cy.intercept({ method: 'GET', url: '/*/v1/lpas/*/complaints' }).as('complaintsRequest');
+  it('should add and edit a complaint', () => {
+    cy.intercept({ method: 'GET', url: '/*/v1/persons/*' }).as('personRequest');
+    cy.intercept({ method: 'GET', url: '/*/v1/lpas/*/complaints' }).as('complaintsRequest');
 
-//     cy.wait(['@complaintsRequest', '@personRequest']);
+    cy.wait(['@complaintsRequest', '@personRequest']);
 
-//     cy.get('#AddComplaint').click();
-//     cy.contains('label', 'Minor').click();
-//     cy.get('#summary0').type('Hey');
-//     cy.get('#description0').type('You know');
-//     cy.get('#SaveAndExit').click();
+    cy.get('#AddComplaint').click();
 
-//     cy.wait('@complaintsRequest');
-//     cy.contains('.timeline-event', 'Complaint').should('contain', 'You know');
+    cy.frameLoaded('.action-widget-content iframe');
+    cy.enter('.action-widget-content iframe').then(getBody => {
+      getBody().find('#f-severity').click();
+      getBody().find('#f-summary').type('Hey');
+      getBody().find('#f-description').type('You know');
+      getBody().find('button[type=submit]').click();
+    });
 
-//     cy.contains('.complaint-item', 'Minor: Hey')
-//       .should('contain', 'You know')
-//       .find('h3').click();
+    cy.wait('@complaintsRequest');
+    cy.contains('.timeline-event', 'Complaint').should('contain', 'You know');
 
-//     cy.contains('label', 'complaint upheld').click();
-//     cy.get('#resolutionDate0').type('01/01/2020');
+    cy.contains('.complaint-item', 'Minor: Hey')
+      .should('contain', 'You know')
+      .find('h3').click();
 
-//     cy.get('#SaveAndExit').click({ force: true });
+    cy.wait(5000);
 
-//     cy.contains('.complaint-item', 'Minor: Hey')
-//       .should('contain', 'You know')
-//       .should('contain', 'complaint upheld on 01/01/2020');
-//   });
-// });
+    cy.frameLoaded('.action-widget-content iframe');
+    cy.enter('.action-widget-content iframe').then(getBody => {
+      getBody().find('#f-resolution').click();
+      getBody().find('#f-resolutionDate').type('2020-01-01');
+      getBody().find('button[type=submit]').click();
+    });
+
+    cy.wait('@complaintsRequest');
+    cy.contains('.complaint-item', 'Minor: Hey')
+      .should('contain', 'You know')
+      .should('contain', 'complaint upheld on 01/01/2020');
+  });
+});
