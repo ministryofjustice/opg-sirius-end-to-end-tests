af357e9c2f396a772973b20747e270163601e715
devkokov <dm.kokov@gmail.com>
2022-12-20 16:51:43 +0000

    SW-6147 Test debug

--- cypress/e2e/supervision/help-and-guidance/help-and-guidance.cy.js
+++ cypress/e2e/supervision/help-and-guidance/help-and-guidance.cy.js
@@ -6,8 +6,10 @@ describe("Help and Guidance", { tags: ["@supervision", "@smoke-journey"] }, () =
 
   it("content is accessible when expanded", () => {
     cy.get('#open-help-and-guidance-main-menu-link').click()
-    cy.wait(2000);
+    cy.log(cy.getFromApi("/api/v1/help-url").its("body"))
+    cy.wait(3000);
     cy.frameLoaded(".help-and-guidance__iframe");
+    cy.get(".help-and-guidance__iframe").should('have.attr', 'src', 'someurl')
     cy.enter(".help-and-guidance__iframe").then((getBody) => {
       getBody().find("#menu-item-2682").click();
     });
