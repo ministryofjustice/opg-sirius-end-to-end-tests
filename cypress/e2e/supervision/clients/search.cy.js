28fde1ce078bc73da166ed29779c145caf5d5ca7
Kate <kate.smith@digital.justice.gov.uk>
2023-04-04 10:56:13 +0100

    sw-6482-passes-locally try 40 times ci

--- cypress/e2e/supervision/clients/search.cy.js
+++ cypress/e2e/supervision/clients/search.cy.js
@@ -1,23 +1,27 @@
-before(() => {
+  beforeEach(() => {
   cy.loginAs("Case Manager");
   cy.createAClient();
 });
+
+  Cypress._.times(40, () => {
 
 describe(
   "Search for a client",
   { tags: ["@supervision", "@client", "@smoke-journey"] },
   () => {
-    it("Search for client that does not exist and create a new one", () => {
-      cy.visit("/supervision/#/dashboard");
-      cy.get('@clientCourtReference').then((clientCourtRef) => {
-        cy.get(".search-bar__input").type(clientCourtRef);
-      })
-      cy.contains('Ted Tedson').click();
-      cy.get('@clientCourtReference').then((clientCourtRef) => {
-        cy.get(
-          'div[class="client-summary__cell client-summary__cell--value court-reference-value-in-client-summary"]'
-        ).should("be.visible", clientCourtRef);
-      })
+
+      it("Search for client that does not exist and create a new one", () => {
+        cy.visit("/supervision/#/dashboard");
+        cy.get('@clientCourtReference').then((clientCourtRef) => {
+          cy.get(".search-bar__input").type(clientCourtRef);
+        })
+        cy.contains('Ted Tedson').click();
+        cy.get('@clientCourtReference').then((clientCourtRef) => {
+          cy.get(
+            'div[class="client-summary__cell client-summary__cell--value court-reference-value-in-client-summary"]'
+          ).should("be.visible", clientCourtRef);
+        })
+      });
     });
   }
 );
