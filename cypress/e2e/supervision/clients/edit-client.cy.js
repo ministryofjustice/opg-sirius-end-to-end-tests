64c15483fae4f2ac1a9209bfe6fbc44b03b7dbcd
Sam Joseph <denialanderror@users.noreply.github.com>
2023-04-18 14:08:55 +0100

    Combines/removes duplicate smoke journey tests (#198)

--- cypress/e2e/supervision/clients/edit-client.cy.js
+++ cypress/e2e/supervision/clients/edit-client.cy.js
@@ -15,20 +15,24 @@ const editClient = (isCourtReferenceChanged) => {
     cy.get('input[name="lastName"]').clear();
     cy.get('input[name="lastName"]').should("not.be.disabled").type(lastName);
     cy.get('input[name="memorablePhrase"]').clear();
-    cy.get('input[name="memorablePhrase"]').should("not.be.disabled").type(memorablePhrase);
+    cy.get('input[name="memorablePhrase"]')
+      .should("not.be.disabled")
+      .type(memorablePhrase);
     if (isCourtReferenceChanged) {
       cy.get('input[name="courtReference"]').clear();
-      cy.get('input[name="courtReference"]').should("not.be.disabled").type("00000000");
+      cy.get('input[name="courtReference"]')
+        .should("not.be.disabled")
+        .type("00000000");
     }
     cy.contains("Save & Exit").click();
     if (isCourtReferenceChanged) {
       cy.wait("@getClientCall").then(({ response }) => {
-        expect(response.statusCode).to.be.eq(200)
-        cy.wrap(response.body.caseRecNumber).as('newCourtReference');
+        expect(response.statusCode).to.be.eq(200);
+        cy.wrap(response.body.caseRecNumber).as("newCourtReference");
       });
     }
   });
-}
+};
 
 beforeEach(() => {
   cy.loginAs("Case Manager");
@@ -40,55 +44,51 @@ beforeEach(() => {
 });
 
 describe(
-  "Edit a client",
-  { tags: ["@supervision", "@client", "@smoke-journey"] },
-  () => {
-    it(
-      "Given I'm a Case Manager on Supervision and on the client dashboard page" +
-      "Then the Client Dashboard page loads as expected",
-      () => {
-        editClient(false)
-        cy.get(".title-person-name").contains(`${firstName} ${lastName}`);
-        cy.get(".TABS_CLIENT_SUMMARY").click();
-        cy.get(".client-summary-full-name-value").contains(`${firstName} ${lastName}`);
-        cy.get(".client-summary-memorable-phrase-value").contains(memorablePhrase);
-      }
-    );
-  }
-);
-
-describe(
-  "Edit an existing client smoke journey test",
+  "Edit an existing client",
   { tags: ["@supervision-core", "@client", "@smoke-journey"] },
   () => {
-    it(
-      "Edits an existing client",
-      () => {
-        editClient(true)
-        cy.get('@newCourtReference').then((newCourtReference) => {
-          cy.get(".title-person-name").contains(`${firstName} ${lastName}`);
-          cy.get(".court-reference-value-in-client-summary").contains(newCourtReference);
+    it("Edits an existing client", () => {
+      editClient(true);
+      cy.get("@newCourtReference").then((newCourtReference) => {
+        cy.get(".title-person-name").contains(`${firstName} ${lastName}`);
+        cy.get(".court-reference-value-in-client-summary").contains(
+          newCourtReference
+        );
 
-          cy.get(".TABS_CLIENT_SUMMARY").click();
-          cy.get(".client-summary-full-name-value").contains(`${firstName} ${lastName}`);
-          cy.get(".client-summary-court-reference-value").contains(newCourtReference);
-          cy.get(".client-summary-memorable-phrase-value").contains(memorablePhrase);
+        cy.get(".TABS_CLIENT_SUMMARY").click();
+        cy.get(".client-summary-full-name-value").contains(
+          `${firstName} ${lastName}`
+        );
+        cy.get(".client-summary-court-reference-value").contains(
+          newCourtReference
+        );
+        cy.get(".client-summary-memorable-phrase-value").contains(
+          memorablePhrase
+        );
 
-          cy.get(".TABS_TIMELINELIST").click();
+        cy.get(".TABS_TIMELINELIST").click();
 
-          cy.get(".timeline-event-title", { timeout: 30000 })
-            .should("contain", "Client edited");
+        cy.get(".timeline-event-title", { timeout: 30000 }).should(
+          "contain",
+          "Client edited"
+        );
 
-          cy.get("timeline-generic-changeset").first().within(() => {
-            cy.contains('First name changed from Ted to ' + `${firstName}`);
-            cy.contains('Last name changed from Tedson to ' + `${lastName}`);
-            cy.contains('Memorable phrase set to ' + `${memorablePhrase}`);
-            cy.get('@clientCourtReference').then((courtreference) => {
-              cy.contains('Court reference changed from ' + courtreference + ' to ' + newCourtReference);
+        cy.get("timeline-generic-changeset")
+          .first()
+          .within(() => {
+            cy.contains("First name changed from Ted to " + `${firstName}`);
+            cy.contains("Last name changed from Tedson to " + `${lastName}`);
+            cy.contains("Memorable phrase set to " + `${memorablePhrase}`);
+            cy.get("@clientCourtReference").then((courtreference) => {
+              cy.contains(
+                "Court reference changed from " +
+                  courtreference +
+                  " to " +
+                  newCourtReference
+              );
             });
           });
-        });
-      }
-    );
+      });
+    });
   }
 );
