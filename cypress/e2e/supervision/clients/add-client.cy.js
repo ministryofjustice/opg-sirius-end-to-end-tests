53874de15de9d0d05b67b1278bf465c77fc3641e
samuel.joseph <denialanderror@googlemail.com>
2023-04-27 16:10:23 +0100

    sw-5917: Test added for the new client search where the client already exists

--- cypress/e2e/supervision/clients/add-client.cy.js
+++ cypress/e2e/supervision/clients/add-client.cy.js
@@ -1,9 +1,9 @@
-before(() => {
+beforeEach(() => {
   cy.loginAs("Case Manager");
 });
 
 describe(
-  "Create a new client",
+  "Add Client page",
   { tags: ["@supervision", "@client", "@smoke-journey"] },
   () => {
     it("Search for client that does not exist and create a new one", () => {
@@ -32,6 +32,27 @@ describe(
           'div[class="client-summary__cell client-summary__cell--value court-reference-value-in-client-summary"]'
         ).should("not.have.value", "00000000");
       });
+    });
+
+    it("Search for a client that exists and navigate to them", () => {
+      cy.createAClient();
+      cy.get("@clientCourtReference").then(courtRef => {
+        cy.visit("/supervision/#/clients/search-for-client");
+
+        cy.get("main")
+          .find('input[placeholder="Search by Order Number, SIRIUS ID or Name"]')
+          .type(courtRef);
+        cy.get('button[class="button client-search__search-button"]').should("be.visible");
+        cy.get('button[class="button client-search__search-button"]').click();
+
+        cy.get('.search-results__list').children().should('have.length', 1)
+          .contains('Ted Tedson')
+          .click();
+
+        cy.get(
+          'div[class="client-summary__cell client-summary__cell--value court-reference-value-in-client-summary"]'
+          ).should("be.visible", courtRef);
+      })
     });
   }
 );
