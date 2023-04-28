9d84e73ed78438a03b5697628d268858ec9d1833
samuel.joseph <denialanderror@googlemail.com>
2023-04-28 10:05:09 +0100

    sw-5917: Adds in a repeater in case the created client isn't available immediately

--- cypress/e2e/supervision/clients/add-client.cy.js
+++ cypress/e2e/supervision/clients/add-client.cy.js
@@ -2,9 +2,27 @@ beforeEach(() => {
   cy.loginAs("Case Manager");
 });
 
+const searchUntilClientFound = (maxAttempts, attempt = 1) => {
+  return cy.then(() => {
+    cy.get("@clientCourtReference").then((courtRef) => {
+      cy.get("main")
+        .find('input[placeholder="Search by Order Number, SIRIUS ID or Name"]')
+        .type(courtRef);
+      cy.get('button[class="button client-search__search-button"]').should("be.visible");
+      cy.get('button[class="button client-search__search-button"]').click();
+      cy.get('.search-results__list').then((results) => {
+        if (results.text().includes("No results could be found") && attempt < maxAttempts) {
+          return searchUntilClientFound(maxAttempts, attempt + 1);
+        }
+        expect(results.text()).to.not.contain("No results could be found");
+      });
+    })
+  });
+}
+
 describe(
   "Add Client page",
-  { tags: ["@supervision", "@client", "@smoke-journey"] },
+  {tags: ["@supervision", "@client", "@smoke-journey"]},
   () => {
     it("Search for client that does not exist and create a new one", () => {
       cy.visit("/supervision/#/clients/search-for-client");
@@ -25,7 +43,7 @@ describe(
 
         cy.contains("Save & exit").click();
 
-        cy.get('span[class="title-person-name"]', { timeout: 30000 })
+        cy.get('span[class="title-person-name"]', {timeout: 30000})
           .should("be.visible")
           .contains(`${client.firstname} ${client.surname}`);
         cy.get(
@@ -36,22 +54,18 @@ describe(
 
     it("Search for a client that exists and navigate to them", () => {
       cy.createAClient();
-      cy.get("@clientCourtReference").then(courtRef => {
-        cy.visit("/supervision/#/clients/search-for-client");
+      cy.visit("/supervision/#/clients/search-for-client");
 
-        cy.get("main")
-          .find('input[placeholder="Search by Order Number, SIRIUS ID or Name"]')
-          .type(courtRef);
-        cy.get('button[class="button client-search__search-button"]').should("be.visible");
-        cy.get('button[class="button client-search__search-button"]').click();
+      searchUntilClientFound(3);
 
-        cy.get('.search-results__list').children().should('have.length', 1)
-          .contains('Ted Tedson')
-          .click();
+      cy.get('.search-results__list').children().should('have.length', 1)
+        .contains('Ted Tedson')
+        .click();
 
+      cy.get('@clientCourtReference').then((clientCourtRef) => {
         cy.get(
           'div[class="client-summary__cell client-summary__cell--value court-reference-value-in-client-summary"]'
-          ).should("be.visible", courtRef);
+        ).should("be.visible", clientCourtRef);
       })
     });
   }
