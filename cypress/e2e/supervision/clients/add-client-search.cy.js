e6036abac53c4b866118b65274ef2c34257310ac
samuel.joseph <denialanderror@googlemail.com>
2023-05-02 11:43:48 +0100

    sw-5917: Navigate to client page first to ensure client exists

--- cypress/e2e/supervision/clients/add-client-search.cy.js
+++ cypress/e2e/supervision/clients/add-client-search.cy.js
@@ -1,5 +1,6 @@
 beforeEach(() => {
   cy.loginAs("Case Manager");
+  cy.createAClient();
 });
 
 const searchUntilClientFound = (maxAttempts, attempt = 1) => {
@@ -22,39 +23,13 @@ const searchUntilClientFound = (maxAttempts, attempt = 1) => {
 }
 
 describe(
-  "Add Client page",
+  "Add Client page - existing client",
   {tags: ["@supervision", "@client", "@smoke-journey"]},
   () => {
-    it("Search for client that does not exist and create a new one", () => {
-      cy.visit("/supervision/#/clients/search-for-client");
-
-      cy.get("main")
-        .find('input[placeholder="Search by Order Number, SIRIUS ID or Name"]')
-        .type("DoNotFindMe");
-      cy.get('button[class="button client-search__search-button"]').should("be.visible");
-      cy.get('button[class="button client-search__search-button"]').click();
-      cy.contains("No results could be found");
-
-      cy.contains("Create a new client").click();
-
-      cy.fixture("client/minimal.json").then((client) => {
-        cy.get('input[name="courtReference"]').type(client.caseRecNumber);
-        cy.get('input[name="firstName"]').type(client.firstname);
-        cy.get('input[name="lastName"]').type(client.surname);
-
-        cy.contains("Save & exit").click();
-
-        cy.get('span[class="title-person-name"]', {timeout: 30000})
-          .should("be.visible")
-          .contains(`${client.firstname} ${client.surname}`);
-        cy.get(
-          'div[class="client-summary__cell client-summary__cell--value court-reference-value-in-client-summary"]'
-        ).should("not.have.value", "00000000");
-      });
-    });
-
     it("Search for a client that exists and navigate to them", () => {
-      cy.createAClient();
+      cy.get('@clientId').then(clientId => {
+        cy.visit(`/supervision/#/clients/${clientId}`);
+      });
       cy.visit("/supervision/#/clients/search-for-client");
 
       searchUntilClientFound(3);
