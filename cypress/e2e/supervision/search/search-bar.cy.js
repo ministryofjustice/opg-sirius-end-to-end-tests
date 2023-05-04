2efb8cc41847c1ead0efcf9307fd0a096fdd60f7
samuel.joseph <denialanderror@googlemail.com>
2023-05-04 15:39:15 +0100

    sw-6098: waitForSearchService command added

--- cypress/e2e/supervision/search/search-bar.cy.js
+++ cypress/e2e/supervision/search/search-bar.cy.js
@@ -1,4 +1,4 @@
-before(() => {
+beforeEach(() => {
   cy.loginAs("Case Manager");
   cy.createAClient();
   cy.get("@clientId").then((clientId) => cy.createOrderForClient(clientId));
@@ -7,49 +7,42 @@ before(() => {
   });
 });
 
-const searchUntilFound = (searchTerm, remaining = 3) => {
-  return cy.then(() => {
-    cy.get(".search-bar__input").clear();
-    cy.get(".search-bar__results").should('not.exist');
-    cy.get(".search-bar__input").type(searchTerm);
-    cy.get(".search-bar__results").then((results) => {
-      if (results.text().includes("No results could be found") && remaining > 0) {
-        return searchUntilFound(searchTerm, remaining--);
-      }
-      expect(results.text()).to.not.contain("No results could be found");
+describe(
+  "Search bar",
+  {tags: ["@supervision", "@client", "@smoke-journey"]},
+  () => {
+    it("Search for client", () => {
+      cy.visit("/supervision/#/dashboard");
+      cy.get('@clientCourtReference').then(courtRef => {
+        cy.waitForSearchService(courtRef, ["Client"]);
+      })
+      cy.get(".search-bar__input").clear();
+      cy.get(".search-bar__input").type(courtRef);
+      cy.get(".search-bar__result-link").contains("Ted Tedson").click();
+      cy.get('@clientCourtReference').then((clientCourtRef) => {
+        cy.get(
+          'div[class="client-summary__cell client-summary__cell--value court-reference-value-in-client-summary"]'
+        ).should("be.visible", clientCourtRef);
+      })
     });
 
-    describe(
-      "Search bar",
-      {tags: ["@supervision", "@client", "@smoke-journey"]},
-      () => {
-        it("Search for client", () => {
-          cy.visit("/supervision/#/dashboard");
-          cy.get('@clientCourtReference').then(courtRef => {
-            searchUntilFound(courtRef);
-          })
-          cy.get(".search-bar__result-link").contains("Ted Tedson").click();
-          cy.get('@clientCourtReference').then((clientCourtRef) => {
-            cy.get(
-              'div[class="client-summary__cell client-summary__cell--value court-reference-value-in-client-summary"]'
-            ).should("be.visible", clientCourtRef);
-          })
+    it("finds the deputy by surname", () => {
+        cy.visit("/supervision/#/dashboard");
+        cy.get('@clientCourtReference').then(courtRef => {
+          cy.waitForSearchService(courtRef, ["Client"]);
         });
 
-        it("finds the deputy by surname", () => {
-            cy.visit("/supervision/#/dashboard");
-            cy.fixture("deputy/minimal.json").then(({firstname, salutation, surname}) => {
-              searchUntilFound(surname);
+        cy.fixture("deputy/minimal.json").then(({firstname, salutation, surname}) => {
+          cy.get(".search-bar__input").clear();
+          cy.get(".search-bar__input").type(surname);
 
-              cy.get('.search-bar__results').within(() => {
-                cy.contains(`${salutation} ${firstname} ${surname} [Deputy]`)
-                  .click();
-              });
+          cy.get('.search-bar__results').within(() => {
+            cy.contains(`${salutation} ${firstname} ${surname} [Deputy]`)
+              .click();
+          });
 
-              cy.get('.banner__deputy-wrap--name').contains(`${salutation} ${firstname} ${surname}`, {matchCase: false});
-            })
-          }
-        );
-      })
-  })
-}
+          cy.get('.banner__deputy-wrap--name').contains(`${salutation} ${firstname} ${surname}`, {matchCase: false});
+        })
+      }
+    );
+});
