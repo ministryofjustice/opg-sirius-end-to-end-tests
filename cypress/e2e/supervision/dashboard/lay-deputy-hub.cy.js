cd36b3f54fe8fbd0f91235318320a79e3b5cb736
Kate <kate.smith@digital.justice.gov.uk>
2023-05-09 14:09:09 +0100

    sw-6043 remove run 50 times command as passes in ci

--- cypress/e2e/supervision/dashboard/lay-deputy-hub.cy.js
+++ cypress/e2e/supervision/dashboard/lay-deputy-hub.cy.js
@@ -6,22 +6,21 @@ beforeEach(() => {
     cy.createADeputyAndAssignToExistingOrder(orderId);
   });
 });
-Cypress._.times(50, () => {
-  it("Loads the lay deputy hub", () => {
-    cy.get("@clientId").then((clientId) => cy.visit(`/supervision/#/clients/${clientId}`));
-    cy.get('.TABS_DEPUTIES').click();
-    cy.get('.record').click();
-    cy.url().should('include', 'supervision/#/deputy-hub');
-    cy.get('.deputy-hub-deputy-name-header-link').should('be.visible');
+it("Loads the lay deputy hub", () => {
+  cy.get("@clientId").then((clientId) => cy.visit(`/supervision/#/clients/${clientId}`));
+  cy.get('.TABS_DEPUTIES').click();
+  cy.get('.record').click();
+  cy.url().should('include', 'supervision/#/deputy-hub');
+  cy.get('.deputy-hub-deputy-name-header-link').should('be.visible');
 
-    cy.get('.tab-container__tabs').should('contain.text', 'Details');
-    cy.get('.tab-container__tabs').should('contain.text', 'Clients');
-    cy.get('.tab-container__tabs').should('contain.text', 'Tasks');
-    cy.get('.tab-container__tabs').should('contain.text', 'Warnings');
-    cy.get('.tab-container__tabs').should('contain.text', 'Timeline');
-    cy.get('.tab-container__tabs').should('contain.text', 'Documents');
+  cy.get('.tab-container__tabs').should('contain.text', 'Details');
+  cy.get('.tab-container__tabs').should('contain.text', 'Clients');
+  cy.get('.tab-container__tabs').should('contain.text', 'Tasks');
+  cy.get('.tab-container__tabs').should('contain.text', 'Warnings');
+  cy.get('.tab-container__tabs').should('contain.text', 'Timeline');
+  cy.get('.tab-container__tabs').should('contain.text', 'Documents');
 
-    cy.get('.deputy-sub-theme').should('be.visible');
-  });
+  cy.get('.deputy-sub-theme').should('be.visible');
 });
+
 
