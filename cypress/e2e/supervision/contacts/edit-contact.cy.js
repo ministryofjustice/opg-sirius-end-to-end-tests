8f764968e27df3b878004d4f2c4099f512b0dac4
samuel.joseph <denialanderror@googlemail.com>
2023-05-18 12:35:57 +0100

    sw-6042: Weights file added

--- cypress/e2e/supervision/contacts/edit-contact.cy.js
+++ cypress/e2e/supervision/contacts/edit-contact.cy.js
@@ -24,7 +24,8 @@ describe(
         .and("contain.text", "There is a problem")
         .and("contain.text", "First name - The input is more than 255 characters long");
 
-      cy.get("input[name=firstName]").clear().type("Edited");
+      cy.get("input[name=firstName]").clear();
+      cy.get("input[name=firstName]").type("Edited");
       cy.get("@editContactButton").click();
 
       cy.get("@contact").then(({surname}) => {
