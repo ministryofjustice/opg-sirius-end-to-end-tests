1f3cf6337af7836e0f04594a566ad5c7db7daa86
Nicholassully <31247234+Nicholassully@users.noreply.github.com>
2023-06-02 11:05:53 +0100

    Sw 6022 adding missing test (#220)
    * sw-6022 adding validation error test #minor
    
    * sw-6022 uncommenting a test

--- cypress/e2e/supervision/report/edit-report-due-date.cy.js
+++ cypress/e2e/supervision/report/edit-report-due-date.cy.js
@@ -1,4 +1,3 @@
-
 beforeEach(() => {
   cy.loginAs("Case Manager");
   cy.createClient()
@@ -37,6 +36,37 @@ describe(
       cy.get('[type="submit"]').click();
       cy.get('.button > span').click();
       cy.get('.report-date-details > :nth-child(2) > .date-item-detail').first().contains('24 Jan 2023');
+      }
+    );
+
+    it("Triggering the validation summary when editing due date on a report in supervision", () => {
+        cy.get("@order").then(({id: orderId}) => {
+          cy.get("@client").then(({id: clientId}) => {
+            cy.visit(
+              `/supervision/#/clients/${clientId}?order=${orderId}`
+            );
+          });
+        });
+        cy.get('.TABS_REPORTS').click();
+        cy.get('.extend-report-due-date-link').first().click();
+        cy.get('#begin-extend-report-due-date-button > span').click();
+        cy.get('.date-input').click().type("24 January 2023");
+        cy.window()
+          .its("tinyMCE")
+          .its("activeEditor")
+          .its("initialized", {timeout: 2000});
+        cy.window().then((win) => {
+          const data =
+            '<p>I am extending the annual report to over the limit of 1001 characters in the note field as there is no need for the user to extend pass this limit. If there is in the future than we will change the limit of course to ensure that they can do their jobs and we are not the blockers. I will have to repeat this paragraph three times to even get to the limit.\n' +
+            'I am extending the annual report to over the limit of 1001 characters in the note field as there is no need for the user to extend pass this limit. If there is in the future than we will change the limit of course to ensure that they can do their jobs and we are not the blockers. I will have to repeat this paragraph three times to even get to the limit.\n' +
+            'I am extending the annual report to over the limit of 1001 characters in the note field as there is no need for the user to extend pass this limit. If there is in the future than we will change the limit of course to ensure that they can do their jobs and we are not the blockers. I will have to repeat this paragraph three times to even get to the limit.</p>';
+          let editor = win.tinymce.activeEditor;
+          editor.dom.createRng();
+          editor.execCommand("mceSetContent", false, data);
+        });
+        cy.get('[type="submit"]').click();
+        cy.get('.button > span').click();
+        cy.get('.validation-summary').should("be.visible")
       }
     );
   });
