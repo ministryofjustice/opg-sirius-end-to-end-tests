ff356c0e6bce162248f418b8759dbbaffcedbd6a
nicholassully <nicholas.sully@digital.justice.gov.uk>
2023-06-22 10:11:14 +0100

    sw-6155 removing error test for a jest test

--- cypress/e2e/supervision/clients/events/events.cy.js
+++ cypress/e2e/supervision/clients/events/events.cy.js
@@ -21,13 +21,5 @@ describe(
         .should("contain", "event has been recorded")
         .should("contain", "Event date");
     });
-
-    it("Error create a client event in Supervision", () => {
-      cy.get('#create-event-button').click();
-      cy.get('*[id^="FIELDLABELSN_"]').first().select(1);
-      cy.get('.fieldset > .ng-untouched').type("A".repeat(256));
-      cy.get('.footer > :nth-child(1) > .button').click();
-      cy.get('.validation-summary').contains("Subject - The input is more than 255 characters long");
-    });
   }
 );
