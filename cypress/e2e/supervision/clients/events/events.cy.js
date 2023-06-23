a7f877d244b7e646b61c8cea95729e4ed9aaadb7
Nicholassully <31247234+Nicholassully@users.noreply.github.com>
2023-06-23 09:02:42 +0100

    sw-6155 adding event tests #minor (#231)
    * sw-6155 adding event tests #minor
    
    * sw-6155 removing error test for a jest test
    
    * sw-6155 changed how we get the save btn

---
+++ cypress/e2e/supervision/clients/events/events.cy.js
@@ -0,0 +1,25 @@
+beforeEach(() => {
+  cy.loginAs("Case Manager");
+  cy.createClient();
+  cy.get("@client").then(({id}) => {
+    cy.visit(`/supervision/#/clients/${id}`);
+  });
+});
+
+describe(
+  "Events",
+  {
+    tags: ["@supervision-core", "@client-risk", "@smoke-journey"],
+  },
+  () => {
+    it("Successfully create a client event in Supervision", () => {
+      cy.get('#create-event-button').click();
+      cy.get('*[id^="FIELDLABELSN_"]').first().select(1);
+      cy.get('.footer .button').click();
+      cy.get('.TABS_TIMELINELIST').click();
+      cy.get('.event-note > .section-content > .wrapper')
+        .should("contain", "event has been recorded")
+        .should("contain", "Event date");
+    });
+  }
+);
