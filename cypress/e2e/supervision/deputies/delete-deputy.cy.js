c5b35b1b1adcd86eaade65842a8dbfcaf8abb827
devkokov <dm.kokov@gmail.com>
2023-06-26 14:13:45 +0100

    SW-6193 Check that the deleted deputy can no longer be found via deputy search

--- cypress/e2e/supervision/deputies/delete-deputy.cy.js
+++ cypress/e2e/supervision/deputies/delete-deputy.cy.js
@@ -19,19 +19,31 @@ describe(
   "Delete a deputy",
   { tags: ["@supervision", "@deputy", "@supervision-core", "@smoke-journey"] },
   () => {
-    it("Should remove the deputy from the client's deputy list", () => {
+    it("Should remove the deputy from the client's deputy list and deputy search", () => {
       cy.get(".TABS_DEPUTIES").click();
       cy.get("#deputies-table tr.summary-row").should("have.length", 2)
-      cy.get("tr.summary-row .deputy-status-on-case").contains("Error")
-        .parent(".summary-row").find("a.deputy-record").click()
-      cy.get("#delete-deputy-button").click()
-      cy.get("delete-deputy-dialog button").contains("Delete Deputy").click()
-      cy.url().should("not.contain", "deputy-hub")
-      cy.get("@client").then(({id}) => {
-        cy.visit(`/supervision/#/clients/${id}`);
-        cy.get(".TABS_DEPUTIES").click();
-        cy.get("#deputies-table tr.summary-row").should("have.length", 1)
-      });
+
+      cy.get("@deputy").then(({salutation, firstname, surname}) => {
+        cy.get("tr.summary-row")
+          .filter(':contains("' + salutation + ' ' + firstname + ' ' + surname + '")')
+          .find("a.deputy-record").click()
+
+        cy.get("#delete-deputy-button").click()
+        cy.get("delete-deputy-dialog button").contains("Delete Deputy").click()
+        cy.url().should("not.contain", "deputy-hub")
+
+        cy.get("@client").then(({id}) => {
+          cy.visit(`/supervision/#/clients/${id}`);
+          cy.get(".TABS_DEPUTIES").click();
+          cy.get("#deputies-table tr.summary-row").should("have.length", 1)
+
+          cy.get('#add-deputy-button').should("be.visible").click();
+          cy.get(".deputy-search__input").type(firstname + " " + surname);
+          cy.get(".deputy-search__form > .button").click();
+          cy.get(".deputy-search__found").should("be.visible")
+            .and("contain.text", "No results could be found")
+        });
+      })
     });
   }
 );
