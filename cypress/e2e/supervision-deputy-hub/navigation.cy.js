1f033b76391263790373c841e586b4b6f322f78d
Kate <kate.smith@digital.justice.gov.uk>
2023-07-04 15:48:23 +0100

    sw-5332 delete test suites for now

--- cypress/e2e/supervision-deputy-hub/navigation.cy.js
+++
@@ -1,9 +0,0 @@
-describe("Navigation", { tags: ["@supervision-deputy-hub", "@smoke-journey"] }, () => {
-  beforeEach(() => {
-    cy.loginAs("Case Manager");
-    cy.createADeputy({
-      'deputyType': {'handle': 'PRO', 'label': 'Professional'},
-      'deputySubType': {'handle': 'PERSON', 'label': 'Person'},
-    });
-  });
-});
--- cypress/e2e/supervision-firm-deputy-hub/navigation.cy.js
+++
@@ -1,7 +0,0 @@
-describe("Navigation", { tags: ["@supervision-firm-deputy-hub", "@smoke-journey"] }, () => {
-  before(() => {
-    cy.loginAs("Case Manager");
-    cy.createAFirm();
-  });
-
-});
