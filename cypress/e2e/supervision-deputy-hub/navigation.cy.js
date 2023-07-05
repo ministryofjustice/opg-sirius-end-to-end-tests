1ebc5848d5eb9669ddb295907dfe323e6f1cc5a7
Kate <kate.smith@digital.justice.gov.uk>
2023-07-05 16:11:04 +0100

    sw-5332 add end to end tests for deputy hub repos

---
+++ cypress/e2e/supervision-deputy-hub/navigation.cy.js
@@ -0,0 +1,10 @@
+describe("Navigation", { tags: ["@supervision-deputy-hub", "@smoke-journey"] }, () => {
+  it("tests header", () => {
+    cy.loginAs("Case Manager");
+    cy.createADeputy({
+      'deputyType': {'handle': 'PRO', 'label': 'Professional'},
+      'deputySubType': {'handle': 'PERSON', 'label': 'Person'},
+    });
+    cy.testHeaderNavigation('deputy-hub');
+  })
+});
--- cypress/e2e/workflow/navigation.cy.js
+++ cypress/e2e/workflow/navigation.cy.js
@@ -1,22 +1,6 @@
 describe("Navigation", { tags: ["@workflow", "@smoke-journey"] }, () => {
-  beforeEach(() => {
+  it("tests header", () => {
     cy.loginAs("Case Manager");
-    cy.visit("/supervision/#/dashboard");
-    cy.get('#hook-workflow-button').click()
-  });
-
-  it("lpa link is clicked", () => {
-    cy.get(':nth-child(1) > .govuk-header__link').click()
-    cy.url().should('include', '/lpa')
-  })
-
-  it("admin link is clicked", () => {
-    cy.get(':nth-child(3) > .govuk-header__link').click()
-    cy.url().should('include', '/admin')
-  })
-
-  it("log out link is clicked", () => {
-    cy.get(':nth-child(4) > .govuk-header__link').click()
-    cy.url().should('include', '/auth/login?loggedout=1')
+    cy.testHeaderNavigation('workflow');
   })
 });
