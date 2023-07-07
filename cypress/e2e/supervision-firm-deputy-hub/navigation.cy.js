2f7da72fcae894810fd51aad619c361c23ce3e34
Kate Smith <52196833+kate-49@users.noreply.github.com>
2023-07-07 08:47:33 +0100

    Sw 5332 add tests for sirius header (#243)
    * sw-5332 add new tests for sirius-header
    
    * sw-5332 refactor e2e test
    
    * sw-5332 remove run 25 times

---
+++ cypress/e2e/supervision-firm-deputy-hub/navigation.cy.js
@@ -0,0 +1,23 @@
+describe("Navigation", { tags: ["@supervision-firm-deputy-hub", "@smoke-journey"] }, () => {
+  beforeEach(() => {
+    cy.loginAs("Case Manager");
+    cy.createAFirm();
+  });
+
+  it("tests header navigation", () => {
+    let expectedHeaderLinks = {
+     "poaUrl": {visible: true, current: false},
+     "supervisionUrl": {visible: false,current: true},
+     "adminUrl": {visible: true, current: false},
+     "signOutUrl": {visible: true,current: false}
+    }
+
+    let expectedNavigationLinks = {
+     "createClientUrl": {visible: true, current: false},
+     "workflowUrl": {visible: true, current: false},
+     "financeUrl": {visible: true, current: false}
+    }
+
+   cy.assertHeaderWorks('firm-hub', expectedHeaderLinks, expectedNavigationLinks);
+ })
+});
