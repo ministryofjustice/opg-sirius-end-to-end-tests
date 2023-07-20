2ab44faaf58c71123f2af69143d43e44f740adba
nicholassully <nicholas.sully@digital.justice.gov.uk>
2023-07-20 09:58:19 +0100

    sw-6171 adding test for adding a bond and assertions for timeline

--- cypress/e2e/supervision/orders/bonds/edit-bond.cy.js
+++ cypress/e2e/supervision/orders/bonds/edit-bond.cy.js
@@ -3,6 +3,7 @@ beforeEach(() => {
   cy.createClient()
     .withOrder()
     .withBond();
+
 });
 
 describe(
@@ -18,14 +19,22 @@ describe(
       cy.get(".TABS_ORDERS").click();
       cy.get("#tab-order-list-in-page-notification").should("contain", "Please wait...");
       cy.get("#tab-order-list-in-page-notification").should("not.contain", "Please wait...");
-
       cy.get(".edit-bond-button").click();
+
       cy.get('#requiredBondAmount').clear();
       cy.get('#requiredBondAmount').type("999999999999")
       cy.contains("button", "Save & exit").click();
       cy.get(".validation-summary")
         .and("contain.text", "There is a problem")
         .and("contain.text", "Required bond amount - The input is not less than '10000000000'");
+
+      cy.get('#requiredBondAmount').clear();
+      cy.get('#requiredBondAmount').type("1000");
+      cy.contains("button", "Save & exit").click();
+      cy.get('.TABS_TIMELINELIST').click();
+      cy.get('.event-bond-edited > .section-content > .wrapper')
+        .should("contain", "Bond edited")
+        .should("contain", "Bond amount required changed from ");
     });
   }
 );
