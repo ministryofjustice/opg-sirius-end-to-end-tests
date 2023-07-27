3744318babf2eee7e5317f57ffa8850308bb91a3
Kate <kate.smith@digital.justice.gov.uk>
2023-07-27 16:27:30 +0100

    sw-6176 change add task to chainable command

--- cypress/e2e/supervision/clients/client-tasks-tab.cy.js
+++ cypress/e2e/supervision/clients/client-tasks-tab.cy.js
@@ -1,7 +1,8 @@
 beforeEach(() => {
   cy.loginAs("Case Manager");
   cy.createClient()
-    .withOrder();
+    .withOrder()
+    .withTask();
 
   cy.get("@client").then(({id}) => {
     cy.visit(`/supervision/#/clients/${id}`);
@@ -13,9 +14,6 @@ describe(
   { tags: ["@supervision", "@supervision-regression", "@client-dashboard"] },
   () => {
     it("shows client tasks as expected and updates them when tasks completed", () => {
-      cy.get("@order").then(({ id: orderId }) => {
-        cy.AddTask(orderId);
-      });
       cy.get('.TABS_CLIENT_TASKS > .tab-container__tab-with-count > span').should('contain.text', 2);
       cy.get('.TABS_CLIENT_TASKS').click();
       cy.get('#client-tasks-list').should('be.visible');
@@ -24,9 +22,9 @@ describe(
       cy.get('.task-columns > :nth-child(3)').should('contain.text', 'Assigned to');
       cy.get('.task-columns > :nth-child(4)').should('contain.text', 'Order');
 
-      cy.get('.task-name').should('contain.text', 'Order - Allocate to team');
-      cy.get('.due-date').should('contain.text', '04/08/2023');
-      cy.get('.assigned-to').should('contain.text', 'Case Owner');
+      cy.get(':nth-child(3) > .task-name').should('contain.text', 'Casework - Reply due');
+      cy.get(':nth-child(3) > :nth-child(2) > .due-date').should('contain.text', '29/03/2025');
+      cy.get(':nth-child(3) > .assigned-to').should('contain.text', 'Attorneyship Investigation Team');
       cy.get('.mark').should('contain.text', 'Complete');
       cy.get('.update-task-button').should('contain.text', 'Update Task');
 
@@ -49,3 +47,4 @@ describe(
       cy.get('.task-name').should('contain.not.text', 'Order - Allocate to team');
     });
   });
+
