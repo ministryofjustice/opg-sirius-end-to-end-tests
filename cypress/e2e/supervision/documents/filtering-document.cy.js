2cb8dc17d2d0ab61968dd0f1f7347bc3e22db47d
Nicholassully <31247234+Nicholassully@users.noreply.github.com>
2023-07-28 11:32:03 +0100

    sw-6178 filtering docs #minor (#257)

---
+++ cypress/e2e/supervision/documents/filtering-document.cy.js
@@ -0,0 +1,41 @@
+import * as path from "path";
+describe(
+  "Viewing the documents tab for the client",
+  { tags: ["@supervision", "@replace-document"] },
+  () => {
+    before(() => {
+      cy.loginAs("Allocations User");
+      cy.createClient();
+    });
+
+    it("filtering the documents tab for the client", () => {
+      cy.loginAs("Case Manager");
+
+      cy.get("@client").then(({ id }) => {
+        cy.visit(`/supervision/#/clients/${id}`);
+      });
+      cy.get('[id="create-event-button"]').click();
+
+      cy.get('[name="fIELDLABELSNOTEFILE"]').selectFile('cypress/fixtures/document/niceFile.txt');
+      cy.get('[name="fIELDLABELSNOTETYPE"]').should('be.visible');
+      cy.get('[name="fIELDLABELSNOTETYPE"]').select('Call', { force: true });
+      cy.get("input[type=radio][data-core-value=OUTGOING]").check({ force: true }).should('be.checked')
+      cy.contains("Save & exit").click();
+
+      cy.get('[id="create-event-button"]').click();
+
+      cy.get('[name="fIELDLABELSNOTEFILE"]').selectFile('cypress/fixtures/document/niceFile.txt');
+      cy.get('[name="fIELDLABELSNOTETYPE"]').should('be.visible');
+      cy.get('[name="fIELDLABELSNOTETYPE"]').select('Order', { force: true });
+      cy.get("input[type=radio][data-core-value=OUTGOING]").check({ force: true }).should('be.checked')
+      cy.contains("Save & exit").click();
+
+
+      cy.get('.TABS_DOCUMENTS').click();
+      cy.get('document-list-item').find('.tab-table').should('have.length', 2);
+
+      cy.get('.filter-check-button').contains('Order').click();
+      cy.get('.button').contains('Apply').click();
+      cy.get('document-list-item').find('.tab-table').should('have.length', 1);
+    });
+  });
