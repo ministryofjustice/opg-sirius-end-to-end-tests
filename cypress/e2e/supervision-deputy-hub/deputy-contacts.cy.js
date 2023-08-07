806e273a843c884a8b69d518235b0a0a6d3c92c3
Joseph Smith <joseph.smith@digital.justice.gov.uk>
2023-08-07 09:24:01 +0100

    SW-5017 Created edit deputy contact timeline test

--- cypress/e2e/supervision-deputy-hub/deputy-contacts.cy.js
+++ cypress/e2e/supervision-deputy-hub/deputy-contacts.cy.js
@@ -19,3 +19,23 @@ describe("Create deputy contact for deputy", () => {
     cy.get('[data-cy="contact-added-event"] > .moj-timeline__description').should("contain", "Telephone: (0121) 071 5088")
   });
 });
+
+describe("Edit deputy contact", () => {
+  it("Adds a new timeline event to the deputy timeline", () => {
+    cy.get("@deputy").then(({ id }) => cy.visit("/supervision/deputies/" + id + "/contacts"));
+    cy.get(':nth-child(4) > .govuk-button--secondary').click();
+
+    cy.get("#f-contactName").type("{selectAll}{backspace}John Smith");
+    cy.get("#f-email").type("{selectAll}{backspace}john.smith@email.com");
+    cy.get('[type="submit"]').click();
+
+    cy.get(':nth-child(3) > .moj-sub-navigation__link').contains("Timeline").click();
+
+    cy.get('[data-cy="contact-edited-event"]').should("exist");
+
+    cy.get('[data-cy="contact-edited-event"] > .moj-timeline__header > .moj-timeline__title').should("contain", "John Smith's details updated");
+
+    cy.get('[data-cy="contact-edited-event"] > .moj-timeline__description').should("contain", "John Smith");
+    cy.get('[data-cy="contact-edited-event"] > .moj-timeline__description').should("contain", "john.smith@email.com");
+  });
+});
