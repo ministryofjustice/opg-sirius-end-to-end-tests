6629fdc51fa14cbb8e3d810f114f37477fe22484
Nicholassully <31247234+Nicholassully@users.noreply.github.com>
2023-08-10 11:02:59 +0100

    sw-6220 warning ui test #minor (#268)
    * sw-6220 warning ui test #minor
    
    * sw-6220 changing nav
    
    * sw-6220 reverting guidance changers
    
    * sw-6220 white spacing

--- cypress/e2e/supervision-deputy-hub/navigation.cy.js
+++ cypress/e2e/supervision-deputy-hub/navigation.cy.js
@@ -1,18 +1,18 @@
-describe("Navigation", { tags: ["@supervision-deputy-hub", "@smoke-journey"] }, () => {
-   beforeEach(() => {
-        cy.loginAs("Case Manager");
-        cy.createADeputy({
-          'deputyType': {'handle': 'PRO', 'label': 'Professional'},
-          'deputySubType': {'handle': 'PERSON', 'label': 'Person'},
-        });
+describe("Navigation", {tags: ["@supervision-deputy-hub", "@smoke-journey"]}, () => {
+  beforeEach(() => {
+    cy.loginAs("Case Manager");
+    cy.createADeputy({
+      'deputyType': {'handle': 'PRO', 'label': 'Professional'},
+      'deputySubType': {'handle': 'PERSON', 'label': 'Person'},
     });
+  });
 
    it("tests header navigation", () => {
      let expectedHeaderLinks = {
        "poaUrl": {visible: true, current: false},
-       "supervisionUrl": {visible: false,current: true},
+       "supervisionUrl": {visible: false, current: true},
        "adminUrl": {visible: true, current: false},
-       "signOutUrl": {visible: true,current: false}
+       "signOutUrl": {visible: true, current: false}
      }
 
      let expectedNavigationLinks = {
@@ -21,8 +21,6 @@ describe("Navigation", { tags: ["@supervision-deputy-hub", "@smoke-journey"] }, 
        "financeUrl": {visible: true, current: false},
      }
 
-     cy.assertHeaderWorks('deputy-hub', expectedHeaderLinks, expectedNavigationLinks);
-   })
+    cy.assertHeaderWorks('deputy-hub', expectedHeaderLinks, expectedNavigationLinks);
+  })
 });
-
-
---
+++ cypress/e2e/supervision/clients/add-warning.cy.js
@@ -0,0 +1,40 @@
+beforeEach(() => {
+  cy.loginAs("Case Manager");
+  cy.createClient();
+});
+
+describe(
+  "Add a warning",
+  { tags: ["@supervision", "client", "@smoke-journey"] },
+  () => {
+    it("successfully after checking you cant with out each mandatory fields", () => {
+      cy.get("@client").then(({id}) => {
+        cy.visit("/supervision/#/clients/" + id);
+      });
+      cy.get('#add-warning-button').click();
+      cy.contains("Save & exit").should('be.disabled');
+
+      cy.waitForTinyMCE()
+        .enterText('<p>Warning has been added</p>');
+      cy.contains("Save & exit").should('be.disabled');
+
+      cy.contains("Warning type")
+        .closest(".fieldset")
+        .find("select")
+        .select("1")
+        .contains("Compensation Claim Pending");
+      cy.waitForTinyMCE()
+        .enterText('');
+      cy.contains("Save & exit").should('be.disabled');
+
+      cy.waitForTinyMCE()
+        .enterText('<p>Warning has been added</p>');
+      cy.contains("Save & exit").click();
+
+      cy.get('div.panel > in-page-notification > .in-page-banner').contains('Warning created, redirecting...')
+      cy.get(".TABS_CLIENT_SUMMARY").click();
+      cy.get('.warnings-list').contains('Compensation Claim Pending');
+      cy.get('.client-priority-info > span').contains('Case has warnings');
+    });
+  }
+);
