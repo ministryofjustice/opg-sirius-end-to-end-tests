3bbcceb4fbb999461c1f1523b67461475b1c4802
Kate Smith <52196833+kate-49@users.noreply.github.com>
2023-09-28 12:08:12 +0100

    sw-6811 add timeouts to tests and run 25 times (#307)
    * sw-6811 add timeouts to tests and run 25 times
    
    * sw-6811 increase timeouts for 2 tests still failing
    
    * sw-6811 remove run 25 from passing test and add interval to still failing
    
    * sw-6811 add wait for elements slightly before issue
    
    * sw-6811 allow 2 retries for 2 very flaky tests
    
    * sw-6811 remove retries from remove contact as it worked better without them
    
    * sw-6811: Removes 25s run
    
    ---------
    
    Co-authored-by: samuel.joseph <denialanderror@googlemail.com>

--- cypress/e2e/supervision/contacts/remove-contact.cy.js
+++ cypress/e2e/supervision/contacts/remove-contact.cy.js
@@ -1,29 +1,24 @@
 beforeEach(() => {
   cy.loginAs("Case Manager");
-  cy.createClient()
-    .withContact();
-  cy.get("@client").then(({id}) => {
-    cy.visit(
-      `/supervision/#/clients/${id}`
-    );
+  cy.createClient().withContact();
+  cy.get("@client").then(({ id }) => {
+    cy.visit(`/supervision/#/clients/${id}`);
   });
   cy.get(".TABS_CONTACTS button").click();
 });
 
 describe(
   "Remove contact",
-  {tags: ["@supervision-core", "@contact", "@smoke-journey"]},
+  { tags: ["@supervision-core", "@contact", "@smoke-journey"] },
   () => {
-    it("can not be deleted due to having a document assoicated to them", () => {
+    it("can not be deleted due to having a document associated to them", () => {
       cy.loginAs("Allocations User");
-      cy.get("@client").then(({id}) => {
-        cy.visit(
-          `/supervision/#/clients/${id}`
-        );
+      cy.get("@client").then(({ id }) => {
+        cy.visit(`/supervision/#/clients/${id}`);
       });
       cy.get("@client").withOrder();
-      cy.get("@order").then(({id: orderId}) => {
-        cy.get("@client").then(({id: clientId}) => {
+      cy.get("@order").then(({ id: orderId }) => {
+        cy.get("@client").then(({ id: clientId }) => {
           cy.visit(
             `/supervision/#/clients/${clientId}/orders/${orderId}/drafts/create/template`
           );
@@ -31,31 +26,42 @@ describe(
       });
       cy.contains(".select-template", "Select a template");
       cy.contains("blank: Blank template").click();
-      cy.get('.selectable-list-select-recipients-list > :nth-child(2)').click()
-      cy.get('#create-letter-button').click()
-      const data =
-        '<p>This is a letter that I have created.</p>';
-      cy.waitForTinyMCE()
-        .enterText(data);
+      cy.get(".selectable-list-select-recipients-list > :nth-child(2)").click();
+      cy.get("#create-letter-button").click();
+      const data = "<p>This is a letter that I have created.</p>";
+      cy.waitForTinyMCE().enterText(data);
 
       cy.contains(".button", "Save draft").click();
       cy.get("#publish-close-button").click();
       cy.get("#preview-publish-button").click();
-      cy.get('.footer > .button').click();
-      cy.get('#publish-close-button').click();
+      cy.get(".footer > .button").click();
+      cy.get("#publish-close-button").click();
       cy.get(".TABS_CONTACTS button").click();
-      cy.get('.delete').click();
-      cy.get('.dialog-footer > .button').click();
-      cy.get('tab-contact-list > .hook-tab-content > :nth-child(1) > .in-page-banner').should("be.visible")
-        .and("contain.text", "This contact cannot be deleted. There’s a draft letter to this person, you need to delete it before you can delete the contact. To do this, go to ‘Retrieve drafts’.")
+      cy.get(".delete").click();
+      cy.get(".dialog-footer > .button").should("be.visible", {
+        timeout: 30000,
+      });
+      cy.get(".dialog-footer > .button").click();
+      cy.get(
+        "tab-contact-list > .hook-tab-content > :nth-child(1) > .in-page-banner"
+      ).should("be.visible", { timeout: 60000, interval: 500 });
+      cy.get(
+        "tab-contact-list > .hook-tab-content > :nth-child(1) > .in-page-banner"
+      ).should(
+        "contain.text",
+        "This contact cannot be deleted. There’s a draft letter to this person, you need to delete it before you can delete the contact. To do this, go to ‘Retrieve drafts’."
+      );
     });
 
     it("can not be deleted", () => {
       cy.get(".TABS_CONTACTS button").click();
-      cy.get('.delete').click();
-      cy.get('.dialog-footer > .button').click();
-      cy.get('tab-contact-list > .hook-tab-content > :nth-child(1) > .in-page-banner > .content > h2').should("be.visible")
-        .and("contain.text", "Mr Ian Contacts has now been deleted")
+      cy.get(".delete").click();
+      cy.get(".dialog-footer > .button").click();
+      cy.get(
+        "tab-contact-list > .hook-tab-content > :nth-child(1) > .in-page-banner > .content > h2"
+      )
+        .should("be.visible")
+        .and("contain.text", "Mr Ian Contacts has now been deleted");
     });
   }
 );
