a565d3ecc30fd7725245ca943f84dcf9ecea2529
Sam Joseph <denialanderror@users.noreply.github.com>
2024-01-10 09:48:37 +0000

    sw-6896: Re-enables add deputy tests and takes out waits (#337)

--- cypress/support/supervision-commands.js
+++
@@ -1,5 +0,0 @@
-Cypress.Commands.add("waitForLoading", (bannerText) => {
-  cy.contains(".in-page-loading-banner", bannerText)
-    .should("be.visible");
-  cy.contains(".in-page-loading-banner", bannerText).should("not.exist");
-});
