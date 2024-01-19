fcac5a7c8b97867a01f4ce43ddede47f090e0e9c
Kate Smith <kate.smith@digital.justice.gov.uk>
2024-01-19 10:53:12 +0000

    sw-6996 remove one more timeout

--- cypress/e2e/supervision/finance/finance-admin.cy.js
+++ cypress/e2e/supervision/finance/finance-admin.cy.js
@@ -101,7 +101,7 @@ describe(
       cy.get("finance-upload-card-payment-data h2").should("be.visible").and("contain.text", "Upload card payment data")
       cy.get('[name="bankFile"]').selectFile('cypress/fixtures/finance/empty.csv');
       cy.intercept("POST", "/supervision-api/v1/finance/reports/card-payments").as("formSubmit");
-      cy.get('.finance-upload-date-form-button > .button', { timeout: 30000 }).should("not.be.disabled");
+      cy.get('.finance-upload-date-form-button > .button').should("not.be.disabled");
       cy.get('.finance-upload-date-form-button > .button').click();
       cy.wait("@formSubmit").its('response.statusCode').should('equal', 400)
       cy.get("validation-summary")
