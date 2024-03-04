129312ef2a2cf84d4b6764890280ff1cf13b2474
Kate Smith <kate.smith@digital.justice.gov.uk>
2024-03-04 16:22:24 +0000

    sw-7025 replace help url with intercept

--- cypress/e2e/supervision/help-and-guidance/help-and-guidance.cy.js
+++ cypress/e2e/supervision/help-and-guidance/help-and-guidance.cy.js
@@ -3,13 +3,27 @@ describe("Help and Guidance", { tags: ["@supervision", "@smoke-journey"] }, () =
     cy.loginAs("Case Manager");
     cy.visit("/supervision/#/dashboard");
 
-    cy.window().then((win) => {
-      cy.stub(win, 'open', url => {
-        win.location.href = helpUrl;
-      }).as("popup")
-    })
-    cy.get('#open-help-and-guidance-main-menu-link').click()
-    cy.get('@popup').should("be.called")
+    // cy.window().then((win) => {
+    //   cy.stub(win, 'open', url => {
+    //     win.location.href = 'http://localhost:8080/help-and-guidance/';
+    //   }).as("popup")
+    // })
+
+    cy.intercept('GET', '/*/v1/help-url', {
+      statusCode: 200,
+      body: {"data":{"url":"\/help-and-guidance\/"}},
+    });
+
+    cy
+      .get('#open-help-and-guidance-main-menu-link')
+      .invoke('attr', 'href')
+      .should('contain', '/help-and-guidance');
+
+    cy.get('#open-help-and-guidance-main-menu-link').click();
+    cy.get('@popup').should("be.called");
+    cy.url().should('contain.text', 'help');
+
     cy.get('h1').should('have.class', "help-header");
+
   })
 });
