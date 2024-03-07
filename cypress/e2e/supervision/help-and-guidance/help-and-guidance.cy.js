a7f21e759c798332559e76c8a58919db931b48b0
Kate Smith <kate.smith@digital.justice.gov.uk>
2024-03-07 13:49:23 +0000

    sw-7025 add assert to check url has changed

--- cypress/e2e/supervision/help-and-guidance/help-and-guidance.cy.js
+++ cypress/e2e/supervision/help-and-guidance/help-and-guidance.cy.js
@@ -1,8 +1,11 @@
 describe("Help and Guidance", { tags: ["@supervision", "@smoke-journey"] }, () => {
+  Cypress._.times(30, () => {
+
     it("content is accessible when expanded", () => {
       cy.loginAs("Case Manager");
       cy.visit("/supervision/#/dashboard");
-
+      cy.get('#open-help-and-guidance-main-menu-link').should('be.visible';
+    )
       cy.get('#open-help-and-guidance-main-menu-link')
         .should('be.visible')
         .then(($a) => {
@@ -11,5 +14,8 @@ describe("Help and Guidance", { tags: ["@supervision", "@smoke-journey"] }, () =
           $a.attr('target', '_self')
         })
         .click()
+
+      cy.url().should('not.contain', 'dashboard');
     });
+  });
 });
