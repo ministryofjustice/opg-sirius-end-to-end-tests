67bafbaf817134965308687b337d517f663e3df0
Kate Smith <kate.smith@digital.justice.gov.uk>
2024-03-07 15:57:05 +0000

    sw-7025 take out run 30 times

--- cypress/e2e/supervision/help-and-guidance/help-and-guidance.cy.js
+++ cypress/e2e/supervision/help-and-guidance/help-and-guidance.cy.js
@@ -1,26 +1,23 @@
 describe("Help and Guidance", { tags: ["@supervision", "@smoke-journey"] }, () => {
-  Cypress._.times(30, () => {
+  it("content is accessible when expanded", () => {
+    cy.loginAs("Case Manager");
+    cy.visit("/supervision/#/dashboard");
 
-    it("content is accessible when expanded", () => {
-      cy.loginAs("Case Manager");
-      cy.visit("/supervision/#/dashboard");
+    cy.intercept({ method: "GET", url: "/*/v1/help-url*" }).as(
+      "helpUrlRequest"
+    );
 
-      cy.intercept({ method: "GET", url: "/*/v1/help-url*" }).as(
-        "helpUrlRequest"
-      );
+    cy.wait("@helpUrlRequest");
 
-      cy.wait("@helpUrlRequest");
+    cy.get('#open-help-and-guidance-main-menu-link')
+      .should('be.visible')
+      .then(($a) => {
+        expect($a).to.have.attr('target', '_blank')
+        // update attr to open in same tab
+        $a.attr('target', '_self')
+      })
+      .click()
 
-      cy.get('#open-help-and-guidance-main-menu-link')
-        .should('be.visible')
-        .then(($a) => {
-          expect($a).to.have.attr('target', '_blank')
-          // update attr to open in same tab
-          $a.attr('target', '_self')
-        })
-        .click()
-
-      cy.url().should('not.contain', 'dashboard');
-    });
+    cy.url().should('not.contain', 'dashboard');
   });
 });
