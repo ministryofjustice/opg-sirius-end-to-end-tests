49f49b1ccbb0a9a41027c7429db407686fb0e15f
Kate Smith <kate.smith@digital.justice.gov.uk>
2024-05-22 16:34:55 +0100

    remove run 10 times

--- cypress/e2e/supervision/report/override-report-type.cy.js
+++ cypress/e2e/supervision/report/override-report-type.cy.js
@@ -15,37 +15,35 @@ beforeEach(() => {
   cy.get('#tab-container').contains('Reports').click();
 });
 
-Cypress._.times(10, () => {
-  describe(
-    "Override report type",
-    { tags: ["@supervision", "@reports", "@override-report-type"] },
-    () => {
-      it("Successfully override a report type", () => {
-        cy.get('#tab-container').contains('Reports').click();
-        cy.wait('@getOrders');
-        cy.wait('@getFinance').then(() => {
-          cy.get('report-summary .report-type').should('not.contain.text', 'OPG102');
-          cy.get('.lodge-report-container', { timeout: 10000 }).should('contain.text', 'Lodge report');
-          cy.waitForStableDOM();
-          cy.contains('Request OPG102').should('be.visible');
-          cy.contains('Request OPG102').click();
-        });
-        cy.get('.head > .title').should('contain.text', 'Request an OPG102 report');
-        cy.get('footer .button.primary')
-          .should('contain.text', 'Save & exit')
-          .and('be.disabled')
-        cy.waitForTinyMCE()
-          .enterText('<p>A reason to override the report type</p>');
-        cy.get('footer .button.primary').click()
-        cy.get('.dialog-header').should('contain.text', 'Override report type');
-        cy.intercept('PUT', '**/override-report-type').as('overrideReportType');
-        cy.get('.hook-modal-confirm').click()
-        cy.wait('@overrideReportType');
-        cy.reload();
-        cy.get('#tab-container').contains('Reports').click();
+describe(
+  "Override report type",
+  { tags: ["@supervision", "@reports", "@override-report-type"] },
+  () => {
+    it("Successfully override a report type", () => {
+      cy.get('#tab-container').contains('Reports').click();
+      cy.wait('@getOrders');
+      cy.wait('@getFinance').then(() => {
+        cy.get('report-summary .report-type').should('not.contain.text', 'OPG102');
+        cy.get('.lodge-report-container', { timeout: 10000 }).should('contain.text', 'Lodge report');
         cy.waitForStableDOM();
-        cy.contains('Cancel OPG102', {timeout: 10000}).should('be.visible');
-        cy.get('report-summary .report-type').should('contain.text', 'OPG102')
+        cy.contains('Request OPG102').should('be.visible');
+        cy.contains('Request OPG102').click();
       });
+      cy.get('.head > .title').should('contain.text', 'Request an OPG102 report');
+      cy.get('footer .button.primary')
+        .should('contain.text', 'Save & exit')
+        .and('be.disabled')
+      cy.waitForTinyMCE()
+        .enterText('<p>A reason to override the report type</p>');
+      cy.get('footer .button.primary').click()
+      cy.get('.dialog-header').should('contain.text', 'Override report type');
+      cy.intercept('PUT', '**/override-report-type').as('overrideReportType');
+      cy.get('.hook-modal-confirm').click()
+      cy.wait('@overrideReportType');
+      cy.reload();
+      cy.get('#tab-container').contains('Reports').click();
+      cy.waitForStableDOM();
+      cy.contains('Cancel OPG102', {timeout: 10000}).should('be.visible');
+      cy.get('report-summary .report-type').should('contain.text', 'OPG102')
     });
-});
+  });
