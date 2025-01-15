746d7529d1618b24d59659609ee9547de40ca902
Kate Smith <kate.smith@digital.justice.gov.uk>
2025-01-15 09:30:25 +0000

    sw-7398 test for pdf doc viewer

---
+++ cypress/e2e/supervision/documents/viewing-document.cy.js
@@ -0,0 +1,36 @@
+const uploadDocument = () => {
+  cy.fixture("document/minimal.json").then((document) => {
+    cy.get("@client").then(({caseRecNumber}) => {
+      document.caseRecNumber = caseRecNumber;
+    });
+    cy.postToApi(`/api/public/v1/documents`, document);
+  });
+};
+
+describe(
+  "Viewing a document in the documents tab for the client",
+  { tags: ["@supervision", "@deleting-document"] },
+  () => {
+
+    beforeEach(() => {
+      cy.loginAs("Allocations User");
+      cy.createClient();
+      cy.loginAs("Public API");
+      uploadDocument();
+    })
+
+    it("allows document to be viewed with PDF viewer", () => {
+      cy.loginAs("System Admin");
+      cy.get("@client").then(({ id }) => {
+        cy.visit(`/supervision/#/clients/${id}`);
+      });
+
+      cy.get('#tab-container').contains('Documents').click();
+      cy.get('document-list-item').find('.tab-table').should('have.length', 1);
+      cy.get('.clear > .dotted-link').click();
+
+      cy.get('.document-viewer__iframe', {timeout: 30000}).should('be.visible');
+      cy.iframe('.document-viewer__iframe', {timeout: 30000}).find('.endOfContent').should('exist');
+      cy.iframe('.document-viewer__iframe', {timeout: 30000}).contains("TEST");
+    });
+  });
