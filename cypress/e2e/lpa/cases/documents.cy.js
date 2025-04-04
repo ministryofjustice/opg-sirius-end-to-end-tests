69a34b78c0394bac7d5a756f99c4531fcefdb1b6
Mabel Li <33980519+mabely@users.noreply.github.com>
2025-04-04 12:26:27 +0100

    vega-2769 - Add cypress test for viewing PDFs (#422)
    * Add cypress test for viewing pdf vega-2769#minor
    
    * Update test to attempt to fix test in Jenkins
    
    * Update parallel-weights.json

--- cypress/e2e/lpa/cases/documents.cy.js
+++ cypress/e2e/lpa/cases/documents.cy.js
@@ -39,4 +39,42 @@ describe("Documents", { tags: ["@lpa", "@smoke-journey"] }, () => {
       getBody().contains("textarea", "explanations of any transactions that are not detailed in their financial records");
     });
   });
+
+  it.only("should display the published document", function () {
+    cy.visit(`/lpa/#/person/${this.donorId}/${this.lpaId}`);
+    cy.waitForStableDOM();
+
+    cy.get("#CreateDocument").click();
+
+    cy.waitForIframe(".action-widget-content iframe", { selector: "#f-templateId" });
+    cy.enter(".action-widget-content iframe").then((getBody) => {
+      getBody().find("#f-templateId").type("IT-BANK-LPA");
+      getBody().find(".autocomplete__menu").contains("IT-BANK-LPA: Letter to bank - LPA").click();
+
+      getBody().contains("button", "Continue").click();
+    });
+
+    cy.waitForIframe(".action-widget-content iframe", { content: "Select a recipient" });
+    cy.enter(".action-widget-content iframe").then((getBody) => {
+      getBody().contains("Select a recipient");
+      getBody().contains("label", "Bob Sponge");
+      getBody().find( "[data-module='recipient-checkbox']").click();
+      getBody().contains("button", "Create draft document").click();
+    });
+
+    cy.waitForIframe(".action-widget-content iframe", { content: "Edit draft document" });
+    cy.enter(".action-widget-content iframe").then((getBody) => {
+      getBody().contains("textarea", "Dear Sponge");
+      getBody().contains("button", "Publish draft").click();
+    });
+
+    cy.waitForStableDOM();
+    cy.get('button[title="Documents associated to this case"]').click();
+    cy.get(".document-list").contains("IT-BANK-LPA").click();
+
+    cy.waitForIframe("#docViewer");
+    cy.enter("iframe#docViewer").then((getBody) => {
+      getBody().contains("Dear Sponge");
+    });
+  });
 });
