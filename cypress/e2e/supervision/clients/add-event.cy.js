6e9ef28e4f937bdaaa11f8e65c4a52620edf97c7
Kate Smith <kate.smith@digital.justice.gov.uk>
2025-06-23 09:58:01 +0100

    sw-7540 original content run 10 times

--- cypress/e2e/supervision/clients/add-event.cy.js
+++ cypress/e2e/supervision/clients/add-event.cy.js
@@ -3,34 +3,36 @@ beforeEach(() => {
   cy.createClient();
 });
 
-describe(
-  "Add event to a client",
-  { tags: ["@supervision", "client", "@smoke-journey", "supervision-notes"] },
-  () => {
-    it("Given I'm a Case Manager on Supervision, when I add an event, then Word formatting is cleaned", () => {
-      cy.get("@client").then(({id, firstname, surname}) => {
-        cy.visit("/supervision/#/clients/" + id);
-        cy.contains(`${firstname} ${surname}`);
-        cy.get('[id="create-event-button"]').click();
-        cy.get(".tox-edit-area__iframe", { timeout: 10000 })
-          .should("be.visible")
-          .scrollIntoView();
+Cypress._.times(10, () => {
+  describe(
+    "Add event to a client",
+    { tags: ["@supervision", "client", "@smoke-journey", "supervision-notes"] },
+    () => {
+      it("Given I'm a Case Manager on Supervision, when I add an event, then Word formatting is cleaned", () => {
+        cy.get("@client").then(({ id, firstname, surname }) => {
+          cy.visit("/supervision/#/clients/" + id);
+          cy.contains(`${firstname} ${surname}`);
+          cy.get('[id="create-event-button"]').click();
+          cy.get(".tox-edit-area__iframe", { timeout: 10000 })
+            .should("be.visible")
+            .scrollIntoView();
 
-        const data =
-          '<p class="MsoNormal" style="margin: 0cm 0cm 11.25pt; font-size: 12pt; font-family: Calibri, sans-serif; text-align: justify; background: white;"><span style="font-size: 10.5pt; font-family: &quot;Open Sans&quot;, sans-serif;">Test</span><span style="font-size: 10.5pt; font-family: &quot;Open Sans&quot;, sans-serif; color: rgb(192, 0, 0);"> this</span><span style="font-size: 10.5pt; font-family: &quot;Open Sans&quot;, sans-serif;"><b> pasted </b>data then.<o:p></o:p></span></p>';
+          const data =
+            '<p class="MsoNormal" style="margin: 0cm 0cm 11.25pt; font-size: 12pt; font-family: Calibri, sans-serif; text-align: justify; background: white;"><span style="font-size: 10.5pt; font-family: &quot;Open Sans&quot;, sans-serif;">Test</span><span style="font-size: 10.5pt; font-family: &quot;Open Sans&quot;, sans-serif; color: rgb(192, 0, 0);"> this</span><span style="font-size: 10.5pt; font-family: &quot;Open Sans&quot;, sans-serif;"><b> pasted </b>data then.<o:p></o:p></span></p>';
 
-        cy.waitForTinyMCE()
-          .pasteText(data)
-          .getContent()
-          .then((content) => {
-            expect(content).to.contain(
-              "<p>Test this<strong> pasted </strong>data then.</p>"
-            );
-            expect(content).to.not.contain("Calibri");
-            expect(content).to.not.contain("MsoNormal");
-            expect(content).to.not.contain("span");
-          })
+          cy.waitForTinyMCE()
+            .pasteText(data)
+            .getContent()
+            .then((content) => {
+              expect(content).to.contain(
+                "<p>Test this<strong> pasted </strong>data then.</p>"
+              );
+              expect(content).to.not.contain("Calibri");
+              expect(content).to.not.contain("MsoNormal");
+              expect(content).to.not.contain("span");
+            })
+        });
       });
-    });
-  }
-);
+    }
+  );
+});
