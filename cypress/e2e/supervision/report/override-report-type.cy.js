cf1d54e9235385be62672a5e8a364ef7ee2a5418
Sam Joseph <denialanderror@users.noreply.github.com>
2025-06-23 16:57:59 +0100

    pfs-319: Removes unnecessary fetch of finance calls (#439)

--- cypress/e2e/supervision/report/override-report-type.cy.js
+++ cypress/e2e/supervision/report/override-report-type.cy.js
@@ -8,7 +8,6 @@ beforeEach(() => {
     cy.get("@client").then(({id: clientId}) => {
       cy.visit(`/supervision/#/clients/${clientId}?order=${orderId}`);
       cy.intercept('GET', '**/orders').as('getOrders');
-      cy.intercept('GET', '**/finance/*').as('getFinance');
     });
   });
   cy.get('#tab-container').contains('Reports').click();
@@ -21,13 +20,11 @@ describe(
     it("Successfully override a report type", () => {
       cy.get('#tab-container').contains('Reports').click();
       cy.wait('@getOrders');
-      cy.wait('@getFinance').then(() => {
-        cy.get('report-summary .report-type').should('not.contain.text', 'OPG102');
-        cy.get('.lodge-report-container', { timeout: 10000 }).should('contain.text', 'Lodge report');
-        cy.waitForStableDOM();
-        cy.contains('Request OPG102').should('be.visible');
-        cy.contains('Request OPG102').click();
-      });
+      cy.get('report-summary .report-type').should('not.contain.text', 'OPG102');
+      cy.get('.lodge-report-container', { timeout: 10000 }).should('contain.text', 'Lodge report');
+      cy.waitForStableDOM();
+      cy.contains('Request OPG102').should('be.visible');
+      cy.contains('Request OPG102').click();
       cy.get('.head > .title').should('contain.text', 'Request an OPG102 report');
       cy.get('footer .button.primary')
         .should('contain.text', 'Save & exit')
