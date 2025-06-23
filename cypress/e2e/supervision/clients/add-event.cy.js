5664d22cedbfa32eaec9279af38bc2ec381aeef4
Kate Smith <kate.smith@digital.justice.gov.uk>
2025-06-23 16:09:40 +0100

    sw-7540 take out run 30 times

--- cypress/e2e/supervision/clients/add-event.cy.js
+++ cypress/e2e/supervision/clients/add-event.cy.js
@@ -2,30 +2,28 @@ beforeEach(() => {
   cy.loginAs("Case Manager");
   cy.createClient();
 });
-Cypress._.times(30, () => {
-  describe(
-    "Add event to a client",
-    { tags: ["@supervision", "client", "@smoke-journey", "supervision-notes"] },
-    () => {
-      it("Given I'm a Case Manager on Supervision, when I add an event, then Word formatting is cleaned", () => {
-        cy.get("@client").then(({ id, firstname, surname }) => {
-          cy.visit("/supervision/#/clients/" + id);
-          cy.contains(`${firstname} ${surname}`);
-          cy.get('[id="create-event-button"]').click();
+describe(
+  "Add event to a client",
+  { tags: ["@supervision", "client", "@smoke-journey", "supervision-notes"] },
+  () => {
+    it("Given I'm a Case Manager on Supervision, when I add an event, then Word formatting is cleaned", () => {
+      cy.get("@client").then(({ id, firstname, surname }) => {
+        cy.visit("/supervision/#/clients/" + id);
+        cy.contains(`${firstname} ${surname}`);
+        cy.get('[id="create-event-button"]').click();
 
-          const data =
-            "<p>Test this<strong> pasted </strong>data then.</p>";
+        const data =
+          "<p>Test this<strong> pasted </strong>data then.</p>";
 
-          cy.getEditorByLabel("Notes (optional)")
-            .pasteText(data)
-            .getContent()
-            .then((content) => {
-              expect(content).to.contain(
-                "<p>Test this<strong> pasted </strong>data then.</p>"
-              );
-            })
-        });
+        cy.getEditorByLabel("Notes (optional)")
+          .pasteText(data)
+          .getContent()
+          .then((content) => {
+            expect(content).to.contain(
+              "<p>Test this<strong> pasted </strong>data then.</p>"
+            );
+          })
       });
-    }
-  );
-});
+    });
+  }
+);
