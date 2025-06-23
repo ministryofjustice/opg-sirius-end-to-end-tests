01f6194f6fbda6980e9a579d19ff7b7c208ddeb6
Kate Smith <kate.smith@digital.justice.gov.uk>
2025-06-23 16:54:42 +0100

    sw-7540 update test and weights

--- cypress/e2e/supervision/clients/add-event.cy.js
+++ cypress/e2e/supervision/clients/add-event.cy.js
@@ -2,12 +2,13 @@ beforeEach(() => {
   cy.loginAs("Case Manager");
   cy.createClient();
 });
+
 describe(
   "Add event to a client",
   { tags: ["@supervision", "client", "@smoke-journey", "supervision-notes"] },
   () => {
     it("Given I'm a Case Manager on Supervision, when I add an event, then Word formatting is cleaned", () => {
-      cy.get("@client").then(({ id, firstname, surname }) => {
+      cy.get("@client").then(({id, firstname, surname}) => {
         cy.visit("/supervision/#/clients/" + id);
         cy.contains(`${firstname} ${surname}`);
         cy.get('[id="create-event-button"]').click();
