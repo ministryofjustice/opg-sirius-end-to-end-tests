7dd0e14caf57cab8f7e7f8012cc70cad793c4eae
Kate Smith <kate.smith5@justice.gov.uk>
2025-11-24 09:50:53 +0000

    sfs-256 restore bond tests

--- cypress/e2e/supervision/orders/bonds/dispense-bond.cy.js
+++ cypress/e2e/supervision/orders/bonds/dispense-bond.cy.js
@@ -1,10 +1,10 @@
-// beforeEach(() => {
-//   cy.loginAs("Case Manager");
-//   cy.createClient()
-//     .withOrder()
-//     .withBond();
-// });
-//
+beforeEach(() => {
+  cy.loginAs("Case Manager");
+  cy.createClient()
+    .withOrder()
+    .withBond();
+});
+
 describe(
   "Dispense bond",
   () => {
@@ -14,38 +14,38 @@ describe(
         openMode: 0,
       },
     }, () => {
-//       cy.get("@client").then(({ id: clientId }) => {
-//         cy.visit(
-//           `/supervision/#/clients/${clientId}`
-//         );
-//       });
-//
-//       cy.get('#tab-container').contains('Orders').click();
-//
-//     cy.get(".edit-bond-button").click();
-//     cy.contains("button", "Dispense with the bond").click();
-//
-//       cy.get("dispense-bond-dialog").contains("button", "Dispense with the bond").click();
-//
-//       const today = new Date();
-//
-//       cy.get('input[name="bondDispenseDischargedDate_day"]').type(1);
-//       cy.get('input[name="bondDispenseDischargedDate_month"]').type((today.getMonth() + 1).toString());
-//       // future date to display the validation box
-//       cy.get('input[name="bondDispenseDischargedDate_year"]').type((today.getFullYear() + 1).toString());
-//
-//       cy.get('.fieldset > fieldset').contains("Court instructions").click();
-//
-//       cy.contains("button", "Save & dispense with the bond").click();
-//
-//       cy.get(".validation-summary").contains(`cannot be in the future`);
-//
-//       cy.get('input[name="bondDispenseDischargedDate_year"]').clear()
-//       cy.get('input[name="bondDispenseDischargedDate_year"]').type(today.getFullYear().toString());
-//
-//       cy.contains("button", "Save & dispense with the bond").click();
-//
-//       cy.contains("bond-details-view", "Bond: dispensed");
+      cy.get("@client").then(({ id: clientId }) => {
+        cy.visit(
+          `/supervision/#/clients/${clientId}`
+        );
+      });
+
+      cy.get('#tab-container').contains('Orders').click();
+
+    cy.get(".edit-bond-button").click();
+    cy.contains("button", "Dispense with the bond").click();
+
+      cy.get("dispense-bond-dialog").contains("button", "Dispense with the bond").click();
+
+      const today = new Date();
+
+      cy.get('input[name="bondDispenseDischargedDate_day"]').type(1);
+      cy.get('input[name="bondDispenseDischargedDate_month"]').type((today.getMonth() + 1).toString());
+      // future date to display the validation box
+      cy.get('input[name="bondDispenseDischargedDate_year"]').type((today.getFullYear() + 1).toString());
+
+      cy.get('.fieldset > fieldset').contains("Court instructions").click();
+
+      cy.contains("button", "Save & dispense with the bond").click();
+
+      cy.get(".validation-summary").contains(`cannot be in the future`);
+
+      cy.get('input[name="bondDispenseDischargedDate_year"]').clear()
+      cy.get('input[name="bondDispenseDischargedDate_year"]').type(today.getFullYear().toString());
+
+      cy.contains("button", "Save & dispense with the bond").click();
+
+      cy.contains("bond-details-view", "Bond: dispensed");
     });
   }
 );
--- cypress/e2e/supervision/orders/bonds/edit-bond.cy.js
+++ cypress/e2e/supervision/orders/bonds/edit-bond.cy.js
@@ -1,11 +1,11 @@
-// beforeEach(() => {
-//   cy.loginAs("Case Manager");
-//   cy.createClient()
-//     .withOrder()
-//     .withBond();
-//
-// });
-//
+beforeEach(() => {
+  cy.loginAs("Case Manager");
+  cy.createClient()
+    .withOrder()
+    .withBond();
+
+});
+
 describe(
   "Edit bond",
   {
@@ -16,30 +16,30 @@ describe(
   },
   () => {
     it("Triggering the validation summary when editing a bond in supervision", () => {
-//       cy.get("@client").then(({ id: clientId }) => {
-//         cy.visit(
-//           `/supervision/#/clients/${clientId}`
-//         );
-//       });
-//
-//       cy.get('#tab-container').contains('Orders').click();
-//       cy.get(".edit-bond-button").click();
-//       cy.reload()
-//       cy.get('#requiredBondAmount', { timeout: 60000 }).should('be.visible');
-//       cy.get('#requiredBondAmount').clear();
-//       cy.get('#requiredBondAmount').type("999999999999")
-//       cy.contains("button", "Save & exit").click();
-//       cy.get(".validation-summary")
-//         .and("contain.text", "There is a problem")
-//         .and("contain.text", "Required bond amount - Values must be less than 10000000000. Received \"999999999999\"");
-//
-//       cy.get('#requiredBondAmount').clear();
-//       cy.get('#requiredBondAmount').type("1000");
-//       cy.contains("button", "Save & exit").click();
-//       cy.get('#tab-container').contains('Timeline').click();
-//       cy.get('.event-bond-edited > .section-content > .wrapper')
-//         .should("contain", "Bond edited")
-//         .should("contain", "Bond amount required changed from ");
+      cy.get("@client").then(({ id: clientId }) => {
+        cy.visit(
+          `/supervision/#/clients/${clientId}`
+        );
+      });
+
+      cy.get('#tab-container').contains('Orders').click();
+      cy.get(".edit-bond-button").click();
+      cy.reload()
+      cy.get('#requiredBondAmount', { timeout: 60000 }).should('be.visible');
+      cy.get('#requiredBondAmount').clear();
+      cy.get('#requiredBondAmount').type("999999999999")
+      cy.contains("button", "Save & exit").click();
+      cy.get(".validation-summary")
+        .and("contain.text", "There is a problem")
+        .and("contain.text", "Required bond amount - Values must be less than 10000000000. Received \"999999999999\"");
+
+      cy.get('#requiredBondAmount').clear();
+      cy.get('#requiredBondAmount').type("1000");
+      cy.contains("button", "Save & exit").click();
+      cy.get('#tab-container').contains('Timeline').click();
+      cy.get('.event-bond-edited > .section-content > .wrapper')
+        .should("contain", "Bond edited")
+        .should("contain", "Bond amount required changed from ");
     });
   }
 );
