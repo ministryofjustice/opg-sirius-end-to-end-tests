00c2df2c67fc75ae943f8c27fee9e188e064bc5d
MishNajam <mishkat.najam@digital.justice.gov.uk>
2026-02-06 14:44:22 +0000

    VEGA-3207 comment out timeline tests

--- cypress/e2e/lpa/cases/timeline.cy.js
+++ cypress/e2e/lpa/cases/timeline.cy.js
@@ -9,21 +9,21 @@ describe("Timeline", { tags: ["@lpa", "@smoke-journey"] }, () => {
     });
   });
 
-  it("should filter events", function () {
-    cy.visit(`/lpa/#/person/${this.donorId}/${this.lpaId}`);
-    cy.waitForStableDOM();
-
-    cy.intercept({ method: "GET", url: "/*/v1/persons/*/events*" }).as(
-      "eventsRequest"
-    );
-    cy.get(".timeline-facets").contains("Task").click();
-
-    cy.wait("@eventsRequest");
-
-    cy.get(".timeline .timeline-event h2").each((h2) => {
-      if (!(h2 instanceof HTMLHeadingElement)) return;
-
-      cy.wrap(h2).contains("Task");
-    });
-  });
+  // it("should filter events", function () {
+  //   cy.visit(`/lpa/#/person/${this.donorId}/${this.lpaId}`);
+  //   cy.waitForStableDOM();
+  //
+  //   cy.intercept({ method: "GET", url: "/*/v1/persons/*/events*" }).as(
+  //     "eventsRequest"
+  //   );
+  //   cy.get(".timeline-facets").contains("Task").click();
+  //
+  //   cy.wait("@eventsRequest");
+  //
+  //   cy.get(".timeline .timeline-event h2").each((h2) => {
+  //     if (!(h2 instanceof HTMLHeadingElement)) return;
+  //
+  //     cy.wrap(h2).contains("Task");
+  //   });
+  // });
 });
