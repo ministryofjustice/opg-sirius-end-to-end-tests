0481ee027db82d6377078024d101f0bc53bf07b5
ramchale <legionsofbob@gmail.com>
2026-03-20 09:39:28 +0000

    Add add_new_client tests

---
+++ playwright/utils/login_as_case_manager.ts
@@ -0,0 +1,14 @@
+import { type BrowserContext, expect, type Page } from "@playwright/test";
+
+export const loginAsCaseManager = async (page: Page, context: BrowserContext): Promise<void> => {
+  await page.goto("/auth/logout");
+  await context.clearCookies();
+
+  await page.goto("/oauth/login");
+
+  const emailInput = page.locator('input[name="email"]');
+  await expect(emailInput).toHaveValue(/@opgtest\.com/);
+  await emailInput.fill("case.manager@opgtest.com");
+  await page.locator('[type="submit"]').click();
+};
+
