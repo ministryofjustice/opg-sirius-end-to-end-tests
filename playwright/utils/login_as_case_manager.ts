c2cf416f4726ee6a3201d7525ed53a3f35476236
ramchale <legionsofbob@gmail.com>
2026-03-25 13:35:40 +0000

    Run headless in Docker and still work on host

--- playwright/utils/login_as_case_manager.ts
+++ playwright/utils/login_as_case_manager.ts
@@ -1,4 +1,31 @@
 import { type BrowserContext, expect, type Page } from "@playwright/test";
+import * as config from "../playwright.config";
+
+async function setupHandlerForAuthRedirectInDocker(page: Page) {
+  // /oauth/login will redirect to where-ever Sirius is set up to redirect to, which is the mock-oauth-provider on
+  // localhost:8080 for local dev. This rewrites the redirect to work in dockers internal network so Sirius can be used
+  // for local development without needing reconfiguring and restarting for the end-to-end tests to run
+  await page.route("**/oauth/login", async (route) => {
+    const response = await route.fetch({ maxRedirects: 0 });
+    expect(response.status()).toBe(302);
+
+    const locationHeader = response.headers()['location'];
+    console.debug("Received redirect to: ", locationHeader);
+    const newLocation = locationHeader.replace("http://localhost:8080", config.default.use.baseURL);
+    console.debug("Rewriting redirect to: ", newLocation);
+
+    await route.fulfill(
+      {
+        "status": 302,
+        "headers": {
+          ...response.headers(),
+          "location": newLocation
+        },
+        "body": await response.body()
+      }
+    )
+  });
+}
 
 export const loginAsCaseManager = async (
   page: Page,
@@ -7,7 +34,12 @@ export const loginAsCaseManager = async (
   await page.goto("/auth/logout");
   await context.clearCookies();
 
-  await page.goto("/oauth/login");
+  if (config.default.use.baseURL.includes("frontend-proxy")) {
+    await setupHandlerForAuthRedirectInDocker(page);
+  }
+
+  const response = await page.goto("/oauth/login");
+  expect(response.status()).toBe(200);
 
   const emailInput = page.locator('input[name="email"]');
   await expect(emailInput).toHaveValue(/@opgtest\.com/);
