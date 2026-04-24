f552e1ef9f95288948776a69cff6b5c8da3a9b32
ramchale <legionsofbob@gmail.com>
2026-04-24 09:34:04 +0100

    Add some help text for dev Make commands

--- Makefile.dev.mk
+++ Makefile.dev.mk
@@ -4,11 +4,11 @@ default: help
 help: ## The help text you're reading
 	@grep --no-filename -E '^[0-9a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
 
-format:
+format: ## Format Playwright test code
 	cd playwright && docker compose run --rm playwright format
 
-run-playwright-ui:
+run-playwright-ui: ## Start container so Playwright tests can be run through the browser on http://localhost:9525
 	cd playwright && docker compose run -p 9525:9525 --rm playwright ui
 
-show-playwright-report:
+show-playwright-report: ## Start server to view Playwright test report on http://localhost:9323
 	cd playwright && docker compose run --rm -p 9323:9323 playwright show-report
