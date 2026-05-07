77873c49294a9eea32cdcd6ec9bc799225ce0576
ramchale <legionsofbob@gmail.com>
2026-05-07 09:40:00 +0100

    Convert Client events to Playwright

---
+++ playwright/tests/supervision/clients/events/events.spec.ts
@@ -0,0 +1,34 @@
+import { expect, test } from "@playwright/test";
+import { createClient } from "../../../../fixtures/client/create_client";
+import { loginAsCaseManager } from "../../../../utils/login_as";
+
+test.describe("Events", () => {
+  test(
+    "Successfully create a client event in Supervision",
+    { tag: "@supervision-core @client-risk @smoke-journey" },
+    async ({ page, context }) => {
+      await loginAsCaseManager(page, context);
+      const client = await createClient(page);
+
+      await page.goto(`/supervision/#/clients/${client.id}`);
+      await page.getByRole("tab", { name: "Documents" }).click();
+
+      await page.locator("#create-event-button").click();
+      await expect(page.locator("#noteDirection")).toContainText("Incoming");
+
+      await page
+        .locator('*[id^="FIELDLABELSN_"]')
+        .first()
+        .selectOption({ index: 1 });
+      await page.getByRole("button", { name: "Save" }).click();
+
+      await page.getByRole("tab", { name: "Timeline" }).click();
+      await expect(
+        page.locator(".event-note > .section-content > .wrapper"),
+      ).toContainText("event has been recorded");
+      await expect(
+        page.locator(".event-note > .section-content > .wrapper"),
+      ).toContainText("Event date");
+    },
+  );
+});
