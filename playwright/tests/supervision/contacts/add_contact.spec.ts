be89e03f0743666f8d7323ec1621642569734ea7
ramchale <legionsofbob@gmail.com>
2026-07-22 16:05:45 +0100

    Add add_contact.spec.ts tags

--- playwright/tests/supervision/contacts/add_contact.spec.ts
+++ playwright/tests/supervision/contacts/add_contact.spec.ts
@@ -20,79 +20,97 @@ test.describe("Add contact", () => {
     await page.locator("#add-contact-button").click();
   });
 
-  test("new client has no contacts", async ({ page }) => {
-    const addContactTitle = page.locator(".add-contact-title");
-    await expect(addContactTitle).toBeVisible();
-    await expect(addContactTitle).toContainText("Create a new contact");
+  test(
+    "new client has no contacts",
+    {
+      tag: "@supervision-core, @contact",
+    },
+    async ({ page }) => {
+      const addContactTitle = page.locator(".add-contact-title");
+      await expect(addContactTitle).toBeVisible();
+      await expect(addContactTitle).toContainText("Create a new contact");
 
-    const createContactButton = page.getByRole("button", {
-      name: "Create contact",
-    });
-    await expect(createContactButton).toBeVisible();
-    await expect(createContactButton).toBeDisabled();
-  });
+      const createContactButton = page.getByRole("button", {
+        name: "Create contact",
+      });
+      await expect(createContactButton).toBeVisible();
+      await expect(createContactButton).toBeDisabled();
+    },
+  );
 
-  test("can add a non organisation contact", async ({ page }) => {
-    await page
-      .locator(".fieldset", { hasText: "Is organisation" })
-      .getByText("No")
-      .click();
+  test(
+    "can add a non organisation contact",
+    {
+      tag: "@supervision-core, @contact, @smoke-journey",
+    },
+    async ({ page }) => {
+      await page
+        .locator(".fieldset", { hasText: "Is organisation" })
+        .getByText("No")
+        .click();
 
-    await page.locator("input[name=firstName]").fill("FirstName");
-    const createContactButton = page.getByRole("button", {
-      name: "Create contact",
-    });
-    await expect(createContactButton).toBeDisabled();
+      await page.locator("input[name=firstName]").fill("FirstName");
+      const createContactButton = page.getByRole("button", {
+        name: "Create contact",
+      });
+      await expect(createContactButton).toBeDisabled();
 
-    await page.locator("input[name=lastName]").fill("A".repeat(256));
-    await createContactButton.click();
+      await page.locator("input[name=lastName]").fill("A".repeat(256));
+      await createContactButton.click();
 
-    const validationSummary = page.locator(".validation-summary");
-    await expect(validationSummary).toContainText("There is a problem");
-    await expect(validationSummary).toContainText(
-      "Last name - The input is more than 255 characters long",
-    );
+      const validationSummary = page.locator(".validation-summary");
+      await expect(validationSummary).toContainText("There is a problem");
+      await expect(validationSummary).toContainText(
+        "Last name - The input is more than 255 characters long",
+      );
 
-    await page.locator("input[name=lastName]").fill("LastName");
-    await createContactButton.click();
+      await page.locator("input[name=lastName]").fill("LastName");
+      await createContactButton.click();
 
-    const contactList = page.locator(
-      "#contacts-list #contact-table .contact-name",
-    );
-    await expect(contactList).toBeVisible();
-    await expect(contactList).toContainText("FirstName LastName");
-  });
+      const contactList = page.locator(
+        "#contacts-list #contact-table .contact-name",
+      );
+      await expect(contactList).toBeVisible();
+      await expect(contactList).toContainText("FirstName LastName");
+    },
+  );
 
-  test("can add an organisation contact", async ({ page }) => {
-    await page
-      .locator(".fieldset", { hasText: "Is organisation" })
-      .getByText("Yes")
-      .click();
+  test(
+    "can add an organisation contact",
+    {
+      tag: "@supervision-core, @contact",
+    },
+    async ({ page }) => {
+      await page
+        .locator(".fieldset", { hasText: "Is organisation" })
+        .getByText("Yes")
+        .click();
 
-    await page
-      .locator("input[name=correspondenceName]")
-      .fill("Correspondence Name");
-    const createContactButton = page.getByRole("button", {
-      name: "Create contact",
-    });
-    await expect(createContactButton).toBeDisabled();
+      await page
+        .locator("input[name=correspondenceName]")
+        .fill("Correspondence Name");
+      const createContactButton = page.getByRole("button", {
+        name: "Create contact",
+      });
+      await expect(createContactButton).toBeDisabled();
 
-    await page.locator("input[name=companyName]").fill("A".repeat(256));
-    await createContactButton.click();
+      await page.locator("input[name=companyName]").fill("A".repeat(256));
+      await createContactButton.click();
 
-    const validationSummary = page.locator(".validation-summary");
-    await expect(validationSummary).toContainText("There is a problem");
-    await expect(validationSummary).toContainText(
-      "Company name - The input is more than 255 characters long",
-    );
+      const validationSummary = page.locator(".validation-summary");
+      await expect(validationSummary).toContainText("There is a problem");
+      await expect(validationSummary).toContainText(
+        "Company name - The input is more than 255 characters long",
+      );
 
-    await page.locator("input[name=companyName]").fill("Company Name");
-    await createContactButton.click();
+      await page.locator("input[name=companyName]").fill("Company Name");
+      await createContactButton.click();
 
-    const contactList = page.locator(
-      "#contacts-list #contact-table .contact-name",
-    );
-    await expect(contactList).toBeVisible();
-    await expect(contactList).toContainText("Correspondence Name");
-  });
+      const contactList = page.locator(
+        "#contacts-list #contact-table .contact-name",
+      );
+      await expect(contactList).toBeVisible();
+      await expect(contactList).toContainText("Correspondence Name");
+    },
+  );
 });
