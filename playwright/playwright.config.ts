8e42dd2802a9fee43e89f2b82e1cb3864facbcee
Andy Price <sixdaysandy@gmail.com>
2026-09-10 13:59:00 +0100

    SP-4063 - Add Playwright config for outputting junit repots to disk #minor

--- playwright/playwright.config.ts
+++ playwright/playwright.config.ts
@@ -14,15 +14,16 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : 2,
+  workers: process.env.PARALLELISATION ? process.env.PARALLELISATION : 2,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [["html", { open: "never" }]],
+  reporter: process.env.CI ? [
+    ['junit', { outputFile: '/test-results/playwright.xml' }],
+    ["html", { outputFolder: '/test-results/playwright-html', open: "never" }]] :
+    [["html", { open: "never" }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: process.env.BASE_URL
-      ? process.env.BASE_URL
-      : "http://localhost:8080",
+    baseURL: process.env.BASE_URL ? process.env.BASE_URL : "http://localhost:8080",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
